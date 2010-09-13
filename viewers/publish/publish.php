efbe72600c7434a47d6e6c4e821cd496292b5bb8
Irik Golka <igolka@sylvester.acl.arts.usyd.edu.au>
2010-09-13 14:22:03 +1000

    publish wizzard restyle

--- viewers/publish/publish.php
+++ viewers/publish/publish.php
@@ -160,12 +160,23 @@
 		along with this program.  If not, see <http://www.gnu.org/licenses/>.
 		-->
 		<title>Heurist</title>
-		<link rel=stylesheet href="<?=HEURIST_SITE_PATH?>common/css/heurist.css">
+		<link rel=stylesheet href="<?=HEURIST_SITE_PATH?>common/css/global.css">
 		<link rel=stylesheet href="<?=HEURIST_SITE_PATH?>common/css/publish.css">
 
-	</head>
-	<body onLoad="init();">
+</head>
+<body onLoad="init();">
 
+	<a id=home-link href="<?= HEURIST_URL_BASE ?>">
+	<div id=logo title="Click the logo at top left of any Heurist page to return to your Favourites"></div>
+	</a>
+	<div id=quicklinks>
+	<div id=quicklink-cell><nobr>
+		<a href="#" onClick="top.HEURIST.util.popupURL(window, '<?=HEURIST_SITE_PATH?>help/about.html'); return false;">About</a> |
+		<a href=http://www.timemap.net/tmissues.php target=_blank>Report a bug</a> |
+		<a id=contact-link href="mailto:<?=HEURIST_MAIL_TO_INFO?>">Contact us</a>
+	</nobr></div>
+	</div>
+	
 		<script src="<?=HEURIST_SITE_PATH?>common/js/heurist.js"></script>	<!-- core HEURIST functions -->
 		<script src="<?=HEURIST_SITE_PATH?>common/php/display-preferences.php<?=(HEURIST_INSTANCE ? "?instance=".HEURIST_INSTANCE : "")?>"></script>	<!-- sets body css classes based on prefs in session -->
 
@@ -217,8 +228,6 @@
 					break;
 				}
 
-
-
 				var e = document.getElementById('preview-frame');
 				var forpreviewframe = '';
 				var publink = '';
@@ -258,7 +267,6 @@
 						document.forceform.submit();
 					}
 				}
-
 			} //eofun
 
 			function createDynaLink(publink){
@@ -279,7 +287,6 @@
 				var newspan = document.createElement('span');
 				newspan.setAttribute("id", "link-span");
 
-
 				//append new div right after embedding code div
 				document.getElementById('emb-span').appendChild(newspan);
 				//append link to div
@@ -287,9 +294,7 @@
 				//add text to div
 				textdiv = document.createTextNode (' [opens new window - copy and paste to your web page].');
 				newspan.appendChild(textdiv);
-
 			}
-
 
 			function forceSearch() {
 				//if unchecked, no forced style
@@ -300,10 +305,7 @@
 				document.forceform.submit();
 				return false;
 			}
-
-
-
-
+			
 			function setContactLink (){
 				document.getElementById('contact-link').href += '?subject=HEURIST+v' + top.HEURIST.VERSION +
 				'+user:' + '<?= get_user_username(); ?>' +
@@ -314,101 +316,62 @@
 
 			-->
 		</script>
-		<table id=page class=expander border=0 cellspacing=0 cellpadding=0>
-			<tr><td id=header>
 
-					<table border=0 cellspacing=0 cellpadding=0 class=expander>
-						<tr>
-							<td id=logo-cell rowspan=2><a href="<?= HEURIST_URL_BASE ?>"><img src="<?= HEURIST_URL_BASE ?>common/images/hlogo-small.jpg" alt=Heurist></a></td>
-							<td id=title-cell rowspan=2>
+<div id=page>
+	<div id=header>
+		<div id=title-cell rowspan=2><!-- PAGE TITLE --></div>
+		<div id=menu-cell></div>
+	</div>
+	<div class="banner">
+		<h2>Publishing wizard</h2><!-- span id=sideheader><a  href="<?=HEURIST_SITE_PATH?>help/webhelp/index.html?Publish" target="_blank">case study</a></span -->
+	</div>
+		<div class="wizard-box roundedBoth">
+			Select output style:
+				<form  id="forceform" name="forceform" method="post" >
+					<select id="style_select" style="width: 180px;" onChange="setStyle(this.options[this.selectedIndex].value, true);">
+					<? fill_styles();?>
+					</select>
+					<input type="hidden" name="op" value="force">
+					<!-- default style -->
+					<input type="hidden" id="force_args" name="force_args" value="html">
+					<span style="padding: 5px;"></span><input title="Forces search to be obligatorily processed through the specified style" type="checkbox" name="chk_force" id="chk_force" <?= (@$forced ? 'checked' : '') ?>  onClick="forceSearch();">
+					Lock the output to be displayed in this style
+					<span style="padding: 5px;"><img src="<?= HEURIST_URL_BASE ?>common/images/lb.gif" align="top"><a  href="<?=HEURIST_SITE_PATH?>help/webhelp/index.html?Publish" target="_blank">adding stylesheet</a></span>
+				</form>
+		
+							</div>
+		<div class="breaker"></div>
+		<div class="breaker"></div>
+			<table cellpadding="0" cellspacing="0">
+				<tr>
+					<td id=wizard-subheading>Preview:
+						<div class="breaker"></div>
+					</td>
+					<td width="50px"><span style="padding-left:10px; color:#990000; " id="loading-msg">loading...</span><div class="breaker"></div>
+					</td>
+					<td valign="top"><iframe id="frame-rowcount" name="frame-rowcount" scrolling="no" width="120px;" height="16px;" frameborder="0" style="margin-left: 10px; overflow:hidden;" src="<?='/cocoon'.INSTALL_DIR.'/'. $_REQUEST['pub_id'] ?>/rowcount.xsl"></iframe>
+					</td>
+				</tr>
+			</table>
+			<iframe id="preview-frame" width="100%" height="50%" frameborder="0" style="border-bottom: 1px solid lightgrey; border-left: 1px solid lightgrey; border-right: 1px solid lightgrey; border-top: 1px solid lightgrey; overflow:auto;" src="">
+			</iframe>
+			<div class="breaker"></div>
+			<div class="breaker"></div>
+			<div>
+				<span id=wizard-subheading >Embed the code below in your webpage or use:</span><span style="padding:5px;"></span><span id = "emb-span"></span>
+				<span id ="link-span">
+					<a id="dynalink" href="published.php?pub_id=<?= $_REQUEST['pub_id'] ?><?=(HEURIST_INSTANCE ? "&instance=".HEURIST_INSTANCE : "")?>&style=<?= $force_style; ?>" target="_blank">link to published page</a> [opens new window - copy and paste to your web page].
+				</span>
+			</div>
+			<div class="breaker"></div>
+			<span class="spacer"></span>
+			<textarea id="embed" style="width: 100%; height: 90px;">&lt;iframe width=&quot;100%&quot; height=&quot;100%&quot; frameborder=&quot;0&quot; src=&quot;published.php?pub_id=<?= $_REQUEST['pub_id'] ?>&style=<?= $force_style; ?>&quot;&gt;&lt;/iframe&gt;" </textarea>
+			<div class="breaker"></div><div class="breaker"></div>
+			<div id=query-highlight class=roundedBoth>&nbsp;<img src="<?= HEURIST_URL_BASE ?>common/images/small-magglass.gif" align="absbottom"/> Search query:
+			<?php echo $query;?>
+			</div>
 
-								<!-- PAGE TITLE -->
-								&nbsp;
-
-							</td>
-							<td id=quicklink-cell><nobr>
-									<a href="#" onClick="top.HEURIST.util.popupURL(window, '<?=HEURIST_SITE_PATH?>help/about.html'); return false;">About</a> |
-									<a href=http://www.timemap.net/tmissues.php target=_blank>Report a bug</a> |
-									<a id=contact-link href="mailto:<?=HEURIST_MAIL_TO_INFO?>">Contact us</a>
-							</nobr></td>
-						</tr>
-						<tr><td id=menu-cell>
-
-								<!-- MENU -->
-								&nbsp;
-
-						</td></tr>
-					</table>
-			</td></tr>
-			<tr><td id=header-bottom></td></tr>
-			<tr><td id=toolbar>
-
-					<!-- TOOLBAR -->
-
-			</td></tr>
-			<tr><td id=main><table id=main-table class=expander border=0 cellspacing=0 cellpadding=0>
-						<tr>
-							<td id=right-cell><table id=right-panel class="expander panel" border=0 cellspacing=0 cellpadding=0>
-									<tr>
-										<td class=panel-header>
-
-											<!-- RIGHT PANEL HEADER -->
-											<b>Publishing wizard</b>
-
-										</td>
-										<td class=panel-header id=sideheader><a  href="<?=HEURIST_SITE_PATH?>help/webhelp/index.html?Publish" target="_blank">case study</a></td>
-									</tr>
-									<tr><td class=panel-main colspan="2"><div class="expander panel-main-div" id=right-panel-content>
-											<!-- <div id=right-panel-content class="autosize panel-main-content">  -->
-											<!-- RIGHT PANEL CONTENT -->
-											<div id=pagetext>
-
-												<div id=wizard-subheading >Choose output style:</div>
-
-												<div class=wizard-box>
-													<div class="spacer">
-														<form  id="forceform" name="forceform" method="post" >
-															<select id="style_select" style="width: 180px;" onChange="setStyle(this.options[this.selectedIndex].value, true);">
-																<? fill_styles();?>
-															</select>
-															<input type="hidden" name="op" value="force">
-															<!-- default style -->
-															<input type="hidden" id="force_args" name="force_args" value="html">
-															<span style="padding: 5px;"></span><input title="Forces search to be obligatorily processed through the specified style" type="checkbox" name="chk_force" id="chk_force" <?= (@$forced ? 'checked' : '') ?>  onClick="forceSearch();">
-															Lock the output to be displayed in this style
-															<span style="padding: 5px;"><img src="<?= HEURIST_URL_BASE ?>common/images/lb.gif" align="top"><a  href="<?=HEURIST_SITE_PATH?>help/webhelp/index.html?Publish" target="_blank">adding stylesheet</a></span>
-														</form>
-													</div>
-												</div>
-												<div class="breaker"></div>
-												<div class="breaker"></div>
-												<table cellpadding="0" cellspacing="0">
-													<tr>
-														<td id=wizard-subheading>Preview:
-															<div class="breaker"></div>
-														</td><td width="50px"><span style="padding-left:10px; color:#990000; " id="loading-msg">loading...</span><div class="breaker"></div></td>
-														<td valign="top"><iframe id="frame-rowcount" name="frame-rowcount" scrolling="no" width="120px;" height="16px;" frameborder="0" style="margin-left: 10px; overflow:hidden;" src="<?='/cocoon'.INSTALL_DIR.'/'. $_REQUEST['pub_id'] ?>/rowcount.xsl"></iframe></td>
-
-													</tr>
-												</table>
-												<iframe id="preview-frame" width="100%" height="50%" frameborder="0" style="border-bottom: 1px solid lightgrey; border-left: 1px solid lightgrey; border-right: 1px solid lightgrey; border-top: 1px solid lightgrey; overflow:scroll;" src=""></iframe>
-												<div class="breaker"></div>
-												<div class="breaker"></div>
-												<div><span id=wizard-subheading >Embed the code below in your webpage or use:</span><span style="padding:5px;"></span><span id = "emb-span"></span>
-													<span id ="link-span">
-												<a id="dynalink" href="published.php?pub_id=<?= $_REQUEST['pub_id'] ?><?=(HEURIST_INSTANCE ? "&instance=".HEURIST_INSTANCE : "")?>&style=<?= $force_style; ?>" target="_blank">link to published page</a> [opens new window - copy and paste to your web page].</span></div>
-												<div class="breaker"></div>
-
-												<span class="spacer"></span>
-												<textarea id="embed" style="width: 70%; height: 90px;">&lt;iframe width=&quot;80%&quot; height=&quot;70%&quot; frameborder=&quot;0&quot; src=&quot;published.php?pub_id=<?= $_REQUEST['pub_id'] ?>&style=<?= $force_style; ?>&quot;&gt;&lt;/iframe&gt;" </textarea>
-												<div class="breaker"></div><div class="breaker"></div>
-												<div id=query-highlight>&nbsp;<img src="<?= HEURIST_URL_BASE ?>common/images/small-magglass.gif" align="absbottom"/> Search query:
-													<?php echo $query;?>
-
-												</div>
-												<!--</div> -->
-									</div></td></tr>
-			</table></td></tr></table></td><!-- end of #main --></tr>
-		</table>
-	</body>
+	<!-- end of #main -->
+</div>
+</body>
 </html>
