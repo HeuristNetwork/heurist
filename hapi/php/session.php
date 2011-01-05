1106f9ebc0d58f47ffa03406599a92b6aa08a986
Steve White <stephenawhite@hotmail.com>
2011-01-05 12:11:17 +1100

    rename and moves for HAPI

--- hapi/php/session.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once(dirname(__FILE__)."/../../common/connect/cred.php");
-require_once(dirname(__FILE__)."/../../common/connect/db.php");
-
-
-print_r($_SESSION["pj-party"]);
-
-?>
