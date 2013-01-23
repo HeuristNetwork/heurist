646741c20f3ff004f4e71bd0189ea6fdffa2bec2
Steve White <stephenawhite@hotmail.com>
2013-01-23 15:52:24 +1100

    add defines tool

---
+++ admin/development/testDefinesSetup.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once(dirname(__FILE__).'/../../common/config/initialise.php');
+
+echo "<span style=\"padding-right:73px;\">&nbsp;</span>HEURIST_SERVER_NAME - &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".HEURIST_SERVER_NAME ."<br><br>";
+echo "<span style=\"padding-right:43px;\">&nbsp;</span>HEURIST_DOCUMENT_ROOT - &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;". HEURIST_DOCUMENT_ROOT ."<br><br>";
+echo "<span style=\"padding-right:100px;\">&nbsp;</span>HEURIST_SITE_PATH - &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".HEURIST_SITE_PATH ."<br><br>";
+echo "<span style=\"padding-right:102px;\">&nbsp;</span>HEURIST_BASE_URL - &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".HEURIST_BASE_URL ."<br><br>";
+echo "<span style=\"padding-right:68px;\">&nbsp;</span>HEURIST_UPLOAD_ROOT - &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;". HEURIST_UPLOAD_ROOT ."<br><br>";
+echo "<span style=\"padding-right:83px;\">&nbsp;</span>HEURIST_UPLOAD_DIR - &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;". HEURIST_UPLOAD_DIR ."<br><br>";
+echo "<span style=\"padding-right:8px;\">&nbsp;</span>HEURIST_ICON_BASE_SITE_PATH - &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;". HEURIST_ICON_BASE_SITE_PATH ."<br><br>";
+echo "<span style=\"padding-right:0px;\">&nbsp;</span>HEURIST_URL_BASE_UPLOAD_DIR - &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;". HEURIST_URL_BASE_UPLOAD_DIR ."<br><br>";
+echo "<span style=\"padding-right:60px;\">&nbsp;</span>HEURIST_ICON_SITE_PATH - &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;". HEURIST_ICON_SITE_PATH ."<br><br>";
+echo "<span style=\"padding-right:110px;\">&nbsp;</span>HEURIST_ICON_DIR - &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;". HEURIST_ICON_DIR ."<br><br>";
+echo "<span style=\"padding-right:93px;\">&nbsp;</span>HEURIST_THUMB_DIR - &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;". HEURIST_THUMB_DIR ."<br><br>";
+echo "<span style=\"padding-right:41px;\">&nbsp;</span>HEURIST_THUMB_BASE_URL - &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;". HEURIST_THUMB_BASE_URL ."<br><br>";
+echo "<span style=\"padding-right:69px;\">&nbsp;</span>HEURIST_HML_PUBPATH - &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;". HEURIST_HML_PUBPATH ."<br><br>";
+echo "<span style=\"padding-right:62px;\">&nbsp;</span>HEURIST_HTML_PUBPATH - &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;". HEURIST_HTML_PUBPATH ."<br><br>";
+//echo "". ."<br><br>";
+//echo "". ."<br><br>";
+//echo "". ."<br><br>";
+//echo "". ."<br><br>";
+//echo "". ."<br><br>";
+
+?>
