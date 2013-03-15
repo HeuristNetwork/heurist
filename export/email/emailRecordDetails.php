59194ef0ceaadb99995e45d7bd460cbafe10c165
Steve White <stephenawhite@hotmail.com>
2013-03-15 13:47:18 +1100

    update headers export

--- export/email/emailRecordDetails.php
+++ export/email/emailRecordDetails.php
@@ -15,34 +15,17 @@
 */
 
 /**
-* brief description of file
+* emailRecordDetailsphp, generic function to email a record from a Heurist database (generally to another), AO 2011/06/07
 *
-* @author      Tom Murtagh
-* @author      Kim Jackson
-* @author      Ian Johnson   <ian.johnson@sydney.edu.au>
-* @author      Stephen White   <stephen.white@sydney.edu.au>
 * @author      Artem Osmakov   <artem.osmakov@sydney.edu.au>
 * @copyright   (C) 2005-2013 University of Sydney
 * @link        http://Sydney.edu.au/Heurist
 * @version     3.1.0
 * @license     http://www.gnu.org/licenses/gpl-3.0.txt GNU License 3.0
 * @package     Heurist academic knowledge management system
-* @subpackage  !!!subpackagename for file such as Administration, Search, Edit, Application, Library
+* @subpackage  Export
 */
 
-
-
-	/**
-	* emailRecordDetailsphp, generic function to email a record from a Heurist database (generally to another), AO 2011/06/07
-	* @author: Artem Osmakov
-	* @copyright (C) 2005-2010 University of Sydney Digital Innovation Unit.
-	* @link: http://HeuristScholar.org
-	* @license http://www.gnu.org/licenses/gpl-3.0.txt
-	* @package Heurist academic knowledge management system
-	*
-	* @todo - concer any field type "file" as attachment - currently only #221
-	* @todo - remove files from database and folder after sending
-	**/
 
 	define('SAVE_URI', 'disabled');
 
