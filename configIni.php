4c9b192189224261997dece5cf465503a9ce9576
Ian Johnson <ijohnson222@gmail.com>
2013-05-24 17:45:29 +1000

    Remove files in development, Harvard biblio template, doco in configIni.php

--- configIni.php
+++ configIni.php
@@ -33,17 +33,17 @@
 
 // [server]
 // enter the server name or IP address of your Web server, null will pull SERVER_NAME from the request header
-// for example $serverName = "heuristscholar.org";  Be sure to include teh port if not port 80
+// for example $serverName = "heuristscholar.org";  Be sure to include the port if not port 80
 $serverName = null; // override default taken from request header SERVER_NAME
 
 // [database]
 // enter the host name or IP address of your MySQL server, blank --> localhost
 // for example $dbHost = "heuristscholar.org";  will cause the code to use mysql on the server at heuristscholar.org
-$dbHost = ""; // required
+$dbHost = ""; // leave blank for localhost
 
 // MySQL user with full write (create) access on this database server
 // The default installation of MySql gives you "root" as the master user with whatever password you set up for this,
-// but you can specify another user and password with full access if preferred
+// but you can specify another user and password with full access if preferred. Password cannot be null
 $dbAdminUsername = ""; // required
 $dbAdminPassword = ""; //required
 
@@ -51,48 +51,47 @@ $dbAdminPassword = ""; //required
 // For example, if there is a user account "readonly" with a password "readonlypwd", then you would use:
 // $dbReadonlyUsername = "readonly";
 // $dbAReadonlyPassword = "readonlypwd";
+// Password cannot be null
 $dbReadonlyUsername = ""; // required
 $dbReadonlyPassword = ""; //required
 
 // dbPrefix will be prepended to all database names so that you can easily distinguish Heurist databases on your database server
 // from other MySQL databases. Some Admin tools such as PHPMyAdmin will group databases with common prefixes ending in underscore
-// The prefix may be left blank, in which case nothing is prepended.
-$dbPrefix = "hdb_"; // recommended
+// The prefix may be left blank, in which case nothing is prepended. For practial management we strongly recommend a prefix.
+$dbPrefix = "hdb_"; // strongly recommended
 
 // The name of the default database which will be used if no db= is supplied in the URL.
 // If the users of this server nearly always use a shared database eg. a workgroup database of web bookmarks, bibliographic or other research data,
 // specify its name here. DO NOT include the prefix, this will be added automatically.
 // If defaultDBName is blank, a list of available databases will be displayed on startup if none is specified
-$defaultDBname = ""; // may be left blank
+$defaultDBname = ""; // may be left blank, should not include prefix (see previous))
 
 // The HTTP address:port of teh proxy server that will allow access to the internet for external URI's
 // this address will allow heurist to request content through the firewall for general Internet URI's
 $httpProxy = ""; // blank = assumes direct internet access from server - ok for laptop installations.
 
-// A challenge password for creation of new databsaes. If left blank, any logged in user can create a new database
+// A simple challenge password for creation of new databases. If left blank, any logged in user can create a new database
 $passwordForDatabaseCreation=""; // blank = any logged in user can create
 
 // [folders]
 
 // The default root pathname of a directory where Heurist can store uploaded
-// files eg. images, pdfs. PHP must be able to create subdirectories of this directory
+// files eg. images, pdfs. PHP must have permissions to be able to create subdirectories of this directory
 // for each Heurist database and write files within them.
-// For instance, if you would like to upload to a dbdir under /var/www/myUploadDir then use
+// For instance, if you would like to upload to a database directory under /var/www/myUploadDir then use
 // $defaultRootFileUploadPath = "/var/www/myUploadDir/";
-// Then, when running Heurist with db=main, uploaded files will be loaded into /var/www/myUploadDir/main/
-// defaults DocRoot/HEURIST_FILESTORE/dbname
-$defaultRootFileUploadPath = ""; // recommended
+// Then, when running Heurist with db=xyz, uploaded files will be loaded into /var/www/myUploadDir/xyz/
+$defaultRootFileUploadPath = ""; // recommended, defaults to <web root>/HEURIST_FILESTORE/<dbname>
 
-// The Document root  relative pathname of a directory where Heurist can store uploaded
+// The pathname relative to the document root of a directory where Heurist can store uploaded
 // icons eg. .png for rectypes. PHP must be able to create subdirectories off this directory
-// for each Heurist database and write files within them.
-// For instance, if you would like to upload to /var/www/htdocs/myUploadDir/myDB/rectype-icons/
-// assuming that /var/www/htdocs is the sites document root then use
+// for each Heurist database and write files within them. Record type icons are stored in the rectype-icons subdirectory.
+// For instance, if you would like to upload to /var/www/htdocs/myUploadDir/<dbname>/rectype-icons/
+// assuming that /var/www/htdocs is the sites http document root then use
 // $siteRelativeIconUploadBasePath = "/myUploadDir/";  BE SURE TO INCLUDE THE TRAILING "/"
-// Then, when running Heurist with db=main, uploaded icons will be loaded into
-// /var/www/htdocs/myUploadDir/main/rectype-icons/
-// defaults /HEURIST_FILESTORE/
-$siteRelativeIconUploadBasePath = ""; // recommended
+// Then, when running Heurist with db=xyz, uploaded icons will be loaded
+// into /var/www/htdocs/myUploadDir/xyz/rectype-icons/
+$siteRelativeIconUploadBasePath = ""; // recommended, defaults /HEURIST_FILESTORE/
 
 // [email]
 
@@ -117,8 +116,6 @@ $parentIni = dirname(__FILE__)."/../heuristConfigIni.php";
 if (is_file($parentIni)){
 	include_once($parentIni);
 }
-
-// $defaultRootFileUploadPath = ""; THIS IS REDUNDANT VERSION OF 37 LINES ABOVE
 
 // URL of 3d party website thumbnail service. Heurist can call any thumbnailing service which returns an
 // appropriate JPEG or GIF file when passed the URL of a web page. This may be a thumbnail of a secirity block page
