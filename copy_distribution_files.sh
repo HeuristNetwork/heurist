d5c2527231141241a6cf59169b5b7a636de86266
Ian Johnson <ijohnson222@gmail.com>
2013-12-10 17:26:47 +1100

    Update Heurist distribution packaging script to work in more generic environment

--- copy_distribution_files.sh
+++ copy_distribution_files.sh
@@ -4,47 +4,61 @@ if [ -z $1 ]
    exit
    fi
 
-# RUN THIS FILE FROM AN H3-xx DIRECTORY CONTAINING DESIRED HEURIST CODE
- 
 # copy_distribution_files.sh
 # --------------------------
 # This file copies all necessary (and some uncessary ..) Heurist vsn 3 distribution files 
-# from the current directory on the AeR development machine (sylvester) to a temporary directory in the h3-setup directory
-# Make sure the directory is the up-to-date version you want to package. The script will requrie modification for other servers
-# Ian Johnson May 2013
+# from the current directory on our development machine to a temporary directory in the h3-setup directory
+# Make sure the directory is the up-to-date version you want to package. 
+# The script may require modification for other servers / directory layouts
+
+# RUN THIS FILE FROM AN H3-xx DIRECTORY CONTAINING DESIRED HEURIST CODE
+
+# Expects h3-setup directory in same directory as h3-xx directory
+# Modified 9th dec 2013 to run on new virtual server
+# Ian Johnson Dec 2013
+ 
+echo -e "\n"
+# will only remove from a directory called h3-stup and files in subdirectory specified by the parameter 
+echo Removing existing files from h3-setup/h$1
+rm -rf ../h3-setup/h$1/*
 
 echo -e "\n" 
-echo Removing existing files from /var/www/htdocs/h3-setup/h$1
-rm -rf /var/www/htdocs/h3-setup/h$1/*
+echo copying files to ../h3-setup/h$1
+# Remember to add any new directories here
+cp -r admin ../h3-setup/h$1
+cp -r common ../h3-setup/h$1
+cp -r export ../h3-setup/h$1
+cp -r hapi ../h3-setup/h$1
+cp -r import ../h3-setup/h$1
+cp -r records ../h3-setup/h$1
+cp -r search ../h3-setup/h$1
+cp -r viewers ../h3-setup/h$1
+cp -r applications ../h3-setup/h$1
+cp -r documentation ../h3-setup/h$1
 
-echo -e "\n" 
-echo copying files to /var/www/htdocs/h3-setup/h$1
-cp -r admin /var/www/htdocs/h3-setup/h$1
-cp -r common /var/www/htdocs/h3-setup/h$1
-cp -r export /var/www/htdocs/h3-setup/h$1
-cp -r hapi /var/www/htdocs/h3-setup/h$1
-cp -r import /var/www/htdocs/h3-setup/h$1
-cp -r records /var/www/htdocs/h3-setup/h$1
-cp -r search /var/www/htdocs/h3-setup/h$1
-cp -r viewers /var/www/htdocs/h3-setup/h$1
-cp -r MYSQL /var/www/htdocs/h3-setup/h$1
-cp -r documentation /var/www/htdocs/h3-setup/h$1
+# Dec 2013 What was this for ??? I think it was the stored procedures, now superceded
+# cp -r MYSQL ../h3-setup/h$1
 
-cp -r *.* /var/www/htdocs/h3-setup/h$1
-cp -r /var/www/htdocs/h3/external /var/www/htdocs/h3-setup/h$1
-cp -r /var/www/htdocs/h3/help /var/www/htdocs/h3-setup/h$1 
+# Copy all the files in the root of h3-xx
+cp -r *.* ../h3-setup/h$1
+
+# Copy the simlinked directories for external and help as real directories 
+cp -r ../h3/external ../h3-setup/h$1
+cp -r ../h3/help ../h3-setup/h$1 
 
 # remove the Heurist Project home page, only applicable to HeuristScholar.org
-rm /var/www/htdocs/h3-setup/h$1/index.html
+# Dec 2013: we may be removing thuis anyway from the code as we move to a more managed Heurist web site
+rm ../h3-setup/h$1/index.html
 
+# Now zip it all up as a tarball for distribution on the Google code site
 echo -e "\n" 
-echo create tarball <version>>.tar.bz2
-rm -f /var/www/htdocs/h3-setup/h$1.tar.bz2
-tar -cjf /var/www/htdocs/h3-setup/h$1.tar.bz2 -C /var/www/htdocs/h3-setup/ h$1/
+echo create tarball h$1.tar.bz2
+rm -f ../h3-setup/h$1.tar.bz2
+tar -cjf ../h3-setup/h$1.tar.bz2 -C ../h3-setup/ h$1/
 echo -e "\n" 
 
 echo -e "\n\n\n"
-echo Change to /var/www/htdocs/h3-setup and run the Google Code upload program:
+echo Change to ../h3-setup and run the Google Code upload program:
 echo note: you MUST delete the upload from Google Code if it already exists
 echo -e "\n"
 echo gcode_wiki_upload.py -s \"EDIT IN TITLE HERE\" -p heurist --user=ijohnson222@gmail.com h$1.tar.bz2
