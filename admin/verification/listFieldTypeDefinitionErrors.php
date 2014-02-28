47afa6e956b6f5b517742e5a558d8fd070202ae6
Ian Johnson <ijohnson222@gmail.com>
2014-02-28 15:03:21 +1100

    Improve message on verification of field defintions and auto repair button

--- admin/verification/listFieldTypeDefinitionErrors.php
+++ admin/verification/listFieldTypeDefinitionErrors.php
@@ -1,36 +1,40 @@
 <?php
 
-/*
-* Copyright (C) 2005-2013 University of Sydney
-*
-* Licensed under the GNU License, Version 3.0 (the "License"); you may not use this file except
-* in compliance with the License. You may obtain a copy of the License at
-*
-* http://www.gnu.org/licenses/gpl-3.0.txt
-*
-* Unless required by applicable law or agreed to in writing, software distributed under the License
-* is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
-* or implied. See the License for the specific language governing permissions and limitations under
-* the License.
-*/
+    /*
+    * Copyright (C) 2005-2013 University of Sydney
+    *
+    * Licensed under the GNU License, Version 3.0 (the "License"); you may not use this file except
+    * in compliance with the License. You may obtain a copy of the License at
+    *
+    * http://www.gnu.org/licenses/gpl-3.0.txt
+    *
+    * Unless required by applicable law or agreed to in writing, software distributed under the License
+    * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+    * or implied. See the License for the specific language governing permissions and limitations under
+    * the License.
+    */
 
-/**
-* brief description of file
-*
-* @author      Tom Murtagh
-* @author      Kim Jackson
-* @author      Ian Johnson   <ian.johnson@sydney.edu.au>
-* @author      Stephen White   <stephen.white@sydney.edu.au>
-* @author      Artem Osmakov   <artem.osmakov@sydney.edu.au>
-* @copyright   (C) 2005-2013 University of Sydney
-* @link        http://Sydney.edu.au/Heurist
-* @version     3.1.0
-* @license     http://www.gnu.org/licenses/gpl-3.0.txt GNU License 3.0
-* @package     Heurist academic knowledge management system
-* @subpackage  !!!subpackagename for file such as Administration, Search, Edit, Application, Library
-*/
+    /**
+    * listFieldTypeDefinitionErrors.php - identifies invalid terms and record pointer constraints in field definitions
+    * 
+    * TODO: This largely duplicates similar funciton in listFieldTypeDefinitionErrors.php - the two shoudl be combined
+    *       and fixing of the errors shoudl be automated, possibly with an email to development team so we are aware of problem
+
+    *
+    * @author      Tom Murtagh
+    * @author      Kim Jackson
+    * @author      Ian Johnson   <ian.johnson@sydney.edu.au>
+    * @author      Stephen White   <stephen.white@sydney.edu.au>
+    * @author      Artem Osmakov   <artem.osmakov@sydney.edu.au>
+    * @copyright   (C) 2005-2013 University of Sydney
+    * @link        http://Sydney.edu.au/Heurist
+    * @version     3.1.0
+    * @license     http://www.gnu.org/licenses/gpl-3.0.txt GNU License 3.0
+    * @package     Heurist academic knowledge management system
+    * @subpackage  !!!subpackagename for file such as Administration, Search, Edit, Application, Library
+    */
     require_once('getFieldTypeDefinitionErrors.php');
-    
+
     $lists = getInvalidFieldTypes(null);
     $dtysWithInvalidTerms = $lists["terms"];
     $dtysWithInvalidNonSelectableTerms = $lists["terms_nonselectable"];
@@ -38,28 +42,28 @@
 ?>
 <html>
 
-	<head>
+    <head>
         <script src="../../common/js/utilsUI.js"></script>
-		<script type=text/javascript>
-        
+        <script type=text/javascript>
+
             var Hul = top.HEURIST.util;
 
-			function open_selected() {
-				var cbs = document.getElementsByName('bib_cb');
-				if (!cbs  ||  ! cbs instanceof Array)
-				return false;
-				var ids = '';
-				for (var i = 0; i < cbs.length; i++) {
-					if (cbs[i].checked)
-					ids = ids + cbs[i].value + ',';
-				}
-				var link = document.getElementById('selected_link');
-				if (!link)
-				return false;
-				link.href = '../../search/search.html?db=<?= HEURIST_DBNAME?>&w=all&q=ids:' + ids;
-				return true;
-			}
-            
+            function open_selected() {
+                var cbs = document.getElementsByName('bib_cb');
+                if (!cbs  ||  ! cbs instanceof Array)
+                    return false;
+                var ids = '';
+                for (var i = 0; i < cbs.length; i++) {
+                    if (cbs[i].checked)
+                        ids = ids + cbs[i].value + ',';
+                }
+                var link = document.getElementById('selected_link');
+                if (!link)
+                    return false;
+                link.href = '../../search/search.html?db=<?= HEURIST_DBNAME?>&w=all&q=ids:' + ids;
+                return true;
+            }
+
             function onEditFieldType(dty_ID){
 
                 var url = top.HEURIST.basePath + "admin/structure/fields/editDetailType.html?db=<?= HEURIST_DBNAME?>";
@@ -70,116 +74,116 @@
                 }
 
                 top.HEURIST.util.popupURL(top, url,
-                {   "close-on-blur": false,
-                    "no-resize": false,
-                    height: 680,
-                    width: 700,
-                    callback: function(context) {
-                    }
+                    {   "close-on-blur": false,
+                        "no-resize": false,
+                        height: 680,
+                        width: 700,
+                        callback: function(context) {
+                        }
                 });
             }
-		</script>
+        </script>
 
-		<link rel="stylesheet" type="text/css" href="../../common/css/global.css">
-		<link rel="stylesheet" type="text/css" href="../../common/css/admin.css">
-		<style type="text/css">
-			h3, h3 span {
-				display: inline-block;
-				padding:0 0 10px 0;
-			}
-			.msgline {
-				line-height:2em;
-			}
-		</style>
-	</head>
+        <link rel="stylesheet" type="text/css" href="../../common/css/global.css">
+        <link rel="stylesheet" type="text/css" href="../../common/css/admin.css">
+        <style type="text/css">
+            h3, h3 span {
+                display: inline-block;
+                padding:0 0 10px 0;
+            }
+            .msgline {
+                line-height:2em;
+            }
+        </style>
+    </head>
 
-	<body class="popup">
-    
+    <body class="popup">
+
         <script type="text/javascript" src="../../common/js/utilsLoad.js"></script>
         <script type="text/javascript" src="../../common/php/loadCommonInfo.php"></script>
-    
-		<div class="banner">
-			<h2>Invalid Field Type Definition check</h2>
-		</div>
-		<div id="page-inner">
-<?php
-    if (count($dtysWithInvalidTerms)>0 || count($dtysWithInvalidNonSelectableTerms)>0 || count($dtysWithInvalidRectypeConstraint)>0){
-?>
-<script>
-function repairFieldTypes(){
-    
-    function _callback(context){
-        if(top.HEURIST.util.isnull(context) || top.HEURIST.util.isnull(context['result'])){
-            top.HEURIST.util.showError(null);
-        }else{
-            alert(context['result']);
-        }
-    }
-    
-    var dt = [
-<?php   
-        $isfirst = true;
-        foreach ($dtysWithInvalidTerms as $row) {
-            print ($isfirst?"":",")."[".$row['dty_ID'].", 0, '".$row['validTermsString']."']";
-            $isfirst = false;
-        }
-        foreach ($dtysWithInvalidNonSelectableTerms as $row) {
-            print ($isfirst?"":",")."[".$row['dty_ID'].", 1, '".$row['validNonSelTermsString']."']";
-            $isfirst = false;
-        }
-        foreach ($dtysWithInvalidRectypeConstraint as $row) {
-            print ($isfirst?"":",")."[".$row['dty_ID'].", 2, '".$row['validRectypeConstraint']."']";
-            $isfirst = false;
-        }
-?>];
 
-    var str = JSON.stringify(dt);
-    
-    var baseurl = top.HEURIST.baseURL + "admin/verification/repairFieldTypes.php";
-    var callback = _callback;
-    var params = "db=<?= HEURIST_DBNAME?>&data=" + encodeURIComponent(str);
-    top.HEURIST.util.getJsonData(baseurl, callback, params);
-}
-</script>
+        <div class="banner">
+            <h2>Invalid Field Type Definition check</h2>
+        </div>
+        <div id="page-inner">
+            <?php
+                if (count($dtysWithInvalidTerms)>0 || count($dtysWithInvalidNonSelectableTerms)>0 || count($dtysWithInvalidRectypeConstraint)>0){
+                ?>
+                <script>
+                    function repairFieldTypes(){
 
- The following field definitions have inconsistent data. To fix the problem please perform one of follwoing actions:<br /><br /> 
-1. Click hyperlinked name of field name to open an edit form on that Field Type definition. Look edit and save the Terms or Pointer definitions.
-<br />or<br />
-2. Click <button onclick="repairFieldTypes()">Auto Repair</button>
-            <br />
-			<hr/>
-<?php
-						foreach ($dtysWithInvalidTerms as $row) {
-?>
-                    <div class="msgline"><b><a href="#" onclick='{ onEditFieldType(<?= $row['dty_ID'] ?>); return false}'><?= $row['dty_Name'] ?></a></b> field (code <?= $row['dty_ID'] ?>) has 
-<?= count($row['invalidTermIDs'])?> invalid term ID<?=(count($row['invalidTermIDs'])>1?"s":"")?> 
-(code: <?= join(",",$row['invalidTermIDs'])?>)
-                    </div>
-<?php
-						}//for
-						foreach ($dtysWithInvalidNonSelectableTerms as $row) {
-?>
-                    <div class="msgline"><b><a href="#" onclick='{ onEditFieldType(<?= $row['dty_ID'] ?>); return false}'><?= $row['dty_Name'] ?></a></b> field (code <?= $row['dty_ID'] ?>) has 
-<?= count($row['invalidNonSelectableTermIDs'])?> invalid non selectable term ID<?=(count($row['invalidNonSelectableTermIDs'])>1?"s":"")?> 
-(code: <?= join(",",$row['invalidNonSelectableTermIDs'])?>)
-                    </div>
-<?php
-						}
-						foreach ($dtysWithInvalidRectypeConstraint as $row) {
-?>
-                    <div class="msgline"><b><a href="#" onclick='{ onEditFieldType(<?= $row['dty_ID'] ?>); return false}'><?= $row['dty_Name'] ?></a></b> field (code <?= $row['dty_ID'] ?>) has 
-<?= count($row['invalidRectypeConstraint'])?> invalid record type constraint<?=(count($row['invalidRectypeConstraint'])>1?"s":"")?> 
-(code: <?= join(",",$row['invalidRectypeConstraint'])?>)
-                    </div>
-<?php
-						}
+                        function _callback(context){
+                            if(top.HEURIST.util.isnull(context) || top.HEURIST.util.isnull(context['result'])){
+                                top.HEURIST.util.showError(null);
+                            }else{
+                                alert(context['result']);
+                            }
+                        }
 
-    }else{
-        print "All field type definitions are valid";
-    }
-?>            
-            
-		</div>
-	</body>
+                        var dt = [
+                            <?php   
+                                $isfirst = true;
+                                foreach ($dtysWithInvalidTerms as $row) {
+                                    print ($isfirst?"":",")."[".$row['dty_ID'].", 0, '".$row['validTermsString']."']";
+                                    $isfirst = false;
+                                }
+                                foreach ($dtysWithInvalidNonSelectableTerms as $row) {
+                                    print ($isfirst?"":",")."[".$row['dty_ID'].", 1, '".$row['validNonSelTermsString']."']";
+                                    $isfirst = false;
+                                }
+                                foreach ($dtysWithInvalidRectypeConstraint as $row) {
+                                    print ($isfirst?"":",")."[".$row['dty_ID'].", 2, '".$row['validRectypeConstraint']."']";
+                                    $isfirst = false;
+                                }
+                        ?>];
+
+                        var str = JSON.stringify(dt);
+
+                        var baseurl = top.HEURIST.baseURL + "admin/verification/repairFieldTypes.php";
+                        var callback = _callback;
+                        var params = "db=<?= HEURIST_DBNAME?>&data=" + encodeURIComponent(str);
+                        top.HEURIST.util.getJsonData(baseurl, callback, params);
+                    }
+                </script>
+
+                <br/><h3>Warning: Field defintions with unknown codes for terms and/or record types</h3><br/>&nbsp;<br/>
+                
+                The following field definitions have inconsistent data. This is nothing to be concerned about, unless it reoccurs frequently, in which case please advise Heurist developers<br/><br/>
+                To fix the inconsistencies, please click here: <button onclick="repairFieldTypes()">Auto Repair</button>  <br/>&nbsp;<br/>
+                You can also look at the individual field definitions by clicking on the name in the list below<br />&nbsp;<br/>
+                <hr/>
+                <?php
+                    foreach ($dtysWithInvalidTerms as $row) {
+                    ?>
+                    <div class="msgline"><b><a href="#" onclick='{ onEditFieldType(<?= $row['dty_ID'] ?>); return false}'><?= $row['dty_Name'] ?></a></b> field (code <?= $row['dty_ID'] ?>) has 
+                        <?= count($row['invalidTermIDs'])?> invalid term ID<?=(count($row['invalidTermIDs'])>1?"s":"")?> 
+                        (code: <?= join(",",$row['invalidTermIDs'])?>)
+                    </div>
+                    <?php
+                    }//for
+                    foreach ($dtysWithInvalidNonSelectableTerms as $row) {
+                    ?>
+                    <div class="msgline"><b><a href="#" onclick='{ onEditFieldType(<?= $row['dty_ID'] ?>); return false}'><?= $row['dty_Name'] ?></a></b> field (code <?= $row['dty_ID'] ?>) has 
+                        <?= count($row['invalidNonSelectableTermIDs'])?> invalid non selectable term ID<?=(count($row['invalidNonSelectableTermIDs'])>1?"s":"")?> 
+                        (code: <?= join(",",$row['invalidNonSelectableTermIDs'])?>)
+                    </div>
+                    <?php
+                    }
+                    foreach ($dtysWithInvalidRectypeConstraint as $row) {
+                    ?>
+                    <div class="msgline"><b><a href="#" onclick='{ onEditFieldType(<?= $row['dty_ID'] ?>); return false}'><?= $row['dty_Name'] ?></a></b> field (code <?= $row['dty_ID'] ?>) has 
+                        <?= count($row['invalidRectypeConstraint'])?> invalid record type constraint<?=(count($row['invalidRectypeConstraint'])>1?"s":"")?> 
+                        (code: <?= join(",",$row['invalidRectypeConstraint'])?>)
+                    </div>
+                    <?php
+                    }
+
+                }else{
+                    print "<br/><h3>All field type definitions are valid</h3>";
+                }
+            ?>            
+
+        </div>
+    </body>
 </html>
 
--- admin/verification/listFieldTypeDefinitionErrorsCompact.php
+++ admin/verification/listFieldTypeDefinitionErrorsCompact.php
@@ -1,37 +1,41 @@
 <?php
 
-/*
-* Copyright (C) 2005-2013 University of Sydney
-*
-* Licensed under the GNU License, Version 3.0 (the "License"); you may not use this file except
-* in compliance with the License. You may obtain a copy of the License at
-*
-* http://www.gnu.org/licenses/gpl-3.0.txt
-*
-* Unless required by applicable law or agreed to in writing, software distributed under the License
-* is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
-* or implied. See the License for the specific language governing permissions and limitations under
-* the License.
-*/
+    /*
+    * Copyright (C) 2005-2013 University of Sydney
+    *
+    * Licensed under the GNU License, Version 3.0 (the "License"); you may not use this file except
+    * in compliance with the License. You may obtain a copy of the License at
+    *
+    * http://www.gnu.org/licenses/gpl-3.0.txt
+    *
+    * Unless required by applicable law or agreed to in writing, software distributed under the License
+    * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+    * or implied. See the License for the specific language governing permissions and limitations under
+    * the License.
+    */
 
-/**
-* listFieldTypeDefinitionErrorsCompact.php - list for editRecord
-*
-* @author      Tom Murtagh
-* @author      Kim Jackson
-* @author      Ian Johnson   <ian.johnson@sydney.edu.au>
-* @author      Stephen White   <stephen.white@sydney.edu.au>
-* @author      Artem Osmakov   <artem.osmakov@sydney.edu.au>
-* @copyright   (C) 2005-2013 University of Sydney
-* @link        http://Sydney.edu.au/Heurist
-* @version     3.1.0
-* @license     http://www.gnu.org/licenses/gpl-3.0.txt GNU License 3.0
-* @package     Heurist academic knowledge management system
-* @subpackage  !!!subpackagename for file such as Administration, Search, Edit, Application, Library
-*/
+    /**
+    * listFieldTypeDefinitionErrorsCompact.php - identifies invalid terms and record pointer constraints in field definitions
+    * Compact version in popup for editRecord. Normal version is used by Designer View > Utilities
+    * 
+    * TODO: This largely duplicates similar funciton in listFieldTypeDefinitionErrors.php - the two shoudl be combined
+    *       and fixing of the errors shoudl be automated, possibly with an email to development team so we are aware of problem
+    *
+    * @author      Tom Murtagh
+    * @author      Kim Jackson
+    * @author      Ian Johnson   <ian.johnson@sydney.edu.au>
+    * @author      Stephen White   <stephen.white@sydney.edu.au>
+    * @author      Artem Osmakov   <artem.osmakov@sydney.edu.au>
+    * @copyright   (C) 2005-2013 University of Sydney
+    * @link        http://Sydney.edu.au/Heurist
+    * @version     3.1.0
+    * @license     http://www.gnu.org/licenses/gpl-3.0.txt GNU License 3.0
+    * @package     Heurist academic knowledge management system
+    * @subpackage  !!!subpackagename for file such as Administration, Search, Edit, Application, Library
+    */
 
     require_once('getFieldTypeDefinitionErrors.php');
-    
+
     if(@$_REQUEST['data']){
         $lists = json_decode($_REQUEST['data'], true);
     }else{
@@ -44,132 +48,129 @@
             exit();
         }
     }
-    
+
     $dtysWithInvalidTerms = $lists["terms"];
     $dtysWithInvalidNonSelectableTerms = $lists["terms_nonselectable"];
     $dtysWithInvalidRectypeConstraint = $lists["rt_contraints"];
 ?>    
 <html>
 
-	<head>
+    <head>
         <title>Invalid Field Type Definition check</title>
-		<link rel="stylesheet" type="text/css" href="../../common/css/global.css">
-		<link rel="stylesheet" type="text/css" href="../../common/css/admin.css">
+        <link rel="stylesheet" type="text/css" href="../../common/css/global.css">
+        <link rel="stylesheet" type="text/css" href="../../common/css/admin.css">
         <script src="../../common/js/utilsUI.js"></script>
-		<style type="text/css">
-			h3, h3 span {
-				display: inline-block;
-				padding:0 0 10px 0;
-			}
-			.msgline {
-				line-height:2em;
-			}
-		</style>
-	</head>
+        <style type="text/css">
+            h3, h3 span {
+                display: inline-block;
+                padding:0 0 10px 0;
+            }
+            .msgline {
+                line-height:2em;
+            }
+        </style>
+    </head>
 
-	<body class="popup" style="width:95%;">
-    
-		<div id="page-inner" style="top:5px;">
-        
-<script>
-function repairFieldTypes(){
-    
-    function _callback(context){
-        if(top.HEURIST.util.isnull(context) || top.HEURIST.util.isnull(context['result'])){
-            top.HEURIST.util.showError(null);
-        }else{
-            alert(context['result']);
-        }
-    }
-    
-    var dt = [
-<?php   
-        $isfirst = true;
-        foreach ($dtysWithInvalidTerms as $row) {
-            print ($isfirst?"":",")."[".$row['dty_ID'].", 0, '".$row['validTermsString']."']";
-            $isfirst = false;
-        }
-        foreach ($dtysWithInvalidNonSelectableTerms as $row) {
-            print ($isfirst?"":",")."[".$row['dty_ID'].", 1, '".$row['validNonSelTermsString']."']";
-            $isfirst = false;
-        }
-        foreach ($dtysWithInvalidRectypeConstraint as $row) {
-            print ($isfirst?"":",")."[".$row['dty_ID'].", 2, '".$row['validRectypeConstraint']."']";
-            $isfirst = false;
-        }
-?>];
+    <body class="popup" style="width:95%;">
 
-    var str = JSON.stringify(dt);
-    
-    var baseurl = top.HEURIST.baseURL + "admin/verification/repairFieldTypes.php";
-    var callback = _callback;
-    var params = "db=<?= HEURIST_DBNAME?>&data=" + encodeURIComponent(str);
-    top.HEURIST.util.getJsonData(baseurl, callback, params);
-}
-</script>
-        
-        
-The following field definitions have inconsistent data. To fix the problem and remove this message please perform one of follwoing actions:<br /><br /> 
-1. Close this popup, save your edits, then Navigate to Designer View > Structure > Manage Field Types, edit the fields indicated.
-<br />or<br />
-2. Close this popup, save your edits and click "Edit record type" link at top right of edit record form.
-<br />or<br />
-3. Click <button onclick="repairFieldTypes()">Auto Repair</button>
-            <br />
-			<hr/>
-<?php
-if (count($dtysWithInvalidTerms)>0){
-?>
-<!--
-			<div>
-				<h3>Enumeration, Relationship type or Relationship marker field types with invalid terms definitions</h3>
-			</div>
--->            
-<?php
-						foreach ($dtysWithInvalidTerms as $row) {
-?>
-					<div class="msgline"><b><?= $row['dty_Name'] ?></b> field (code <?= $row['dty_ID'] ?>) has 
-<?= count($row['invalidTermIDs'])?> invalid term ID<?=(count($row['invalidTermIDs'])>1?"s":"")?> 
-(code: <?= join(",",$row['invalidTermIDs'])?>)
-					</div>
-<?php
-						}//for
-}
-if (count($dtysWithInvalidNonSelectableTerms)>0){
-?>
-<!--			<div>
-				<h3>Enumeration, Relationship type or Relationship marker field types with invalid non-selectable terms definitions</h3>
-			</div> -->
-					<?php
-						foreach ($dtysWithInvalidNonSelectableTerms as $row) {
-						?>
+        <div id="page-inner" style="top:5px;">
+
+            <script>
+                function repairFieldTypes(){
+
+                    function _callback(context){
+                        if(top.HEURIST.util.isnull(context) || top.HEURIST.util.isnull(context['result'])){
+                            top.HEURIST.util.showError(null);
+                        }else{
+                            alert(context['result']);
+                        }
+                    }
+
+                    var dt = [
+                        <?php   
+                            $isfirst = true;
+                            foreach ($dtysWithInvalidTerms as $row) {
+                                print ($isfirst?"":",")."[".$row['dty_ID'].", 0, '".$row['validTermsString']."']";
+                                $isfirst = false;
+                            }
+                            foreach ($dtysWithInvalidNonSelectableTerms as $row) {
+                                print ($isfirst?"":",")."[".$row['dty_ID'].", 1, '".$row['validNonSelTermsString']."']";
+                                $isfirst = false;
+                            }
+                            foreach ($dtysWithInvalidRectypeConstraint as $row) {
+                                print ($isfirst?"":",")."[".$row['dty_ID'].", 2, '".$row['validRectypeConstraint']."']";
+                                $isfirst = false;
+                            }
+                    ?>];
+
+                    var str = JSON.stringify(dt);
+
+                    var baseurl = top.HEURIST.baseURL + "admin/verification/repairFieldTypes.php";
+                    var callback = _callback;
+                    var params = "db=<?= HEURIST_DBNAME?>&data=" + encodeURIComponent(str);
+                    top.HEURIST.util.getJsonData(baseurl, callback, params);
+                }
+            </script>
+
+
+            <br/><h3>Warning: Field defintions with unknown codes for terms and/or record types</h3><br/>&nbsp;<br/>
+            The following field definitions have inconsistent data. This is nothing to be concerned about, unless it reoccurs frequently, in which case please advise Heurist developers<br/><br/>
+            To fix the inconsistencies, please click here: <button onclick="repairFieldTypes()">Auto Repair</button>  <br/>&nbsp;<br/>
+
+            <hr/>
+            <?php
+                if (count($dtysWithInvalidTerms)>0){
+                ?>
+                <!--
+                <div>
+                <h3>Enumeration, Relationship type or Relationship marker field types with invalid terms definitions</h3>
+                </div>
+                -->            
+                <?php
+                    foreach ($dtysWithInvalidTerms as $row) {
+                    ?>
                     <div class="msgline"><b><?= $row['dty_Name'] ?></b> field (code <?= $row['dty_ID'] ?>) has 
-<?= count($row['invalidNonSelectableTermIDs'])?> invalid non selectable term ID<?=(count($row['invalidNonSelectableTermIDs'])>1?"s":"")?> 
-(code: <?= join(",",$row['invalidNonSelectableTermIDs'])?>)
+                        <?= count($row['invalidTermIDs'])?> invalid term ID<?=(count($row['invalidTermIDs'])>1?"s":"")?> 
+                        (code: <?= join(",",$row['invalidTermIDs'])?>)
                     </div>
-<?php
-						}
-}
-if (count($dtysWithInvalidRectypeConstraint)>0){
-?>
-<!--
-			<div>
-				<h3>Record Pointer or Relationship Marker field types with invalid record type(s) in constraint definitions</h3> 
-			</div> -->
-<?php
-						foreach ($dtysWithInvalidRectypeConstraint as $row) {
-?>
+                    <?php
+                    }//for
+                }
+                if (count($dtysWithInvalidNonSelectableTerms)>0){
+                ?>
+                <!--			<div>
+                <h3>Enumeration, Relationship type or Relationship marker field types with invalid non-selectable terms definitions</h3>
+                </div> -->
+                <?php
+                    foreach ($dtysWithInvalidNonSelectableTerms as $row) {
+                    ?>
                     <div class="msgline"><b><?= $row['dty_Name'] ?></b> field (code <?= $row['dty_ID'] ?>) has 
-<?= count($row['invalidRectypeConstraint'])?> invalid record type constraint<?=(count($row['invalidRectypeConstraint'])>1?"s":"")?> 
-(code: <?= join(",",$row['invalidRectypeConstraint'])?>)
+                        <?= count($row['invalidNonSelectableTermIDs'])?> invalid non selectable term ID<?=(count($row['invalidNonSelectableTermIDs'])>1?"s":"")?> 
+                        (code: <?= join(",",$row['invalidNonSelectableTermIDs'])?>)
                     </div>
-                        
-<?php
-						}
-}
-?>
+                    <?php
+                    }
+                }
+                if (count($dtysWithInvalidRectypeConstraint)>0){
+                ?>
+                <!--
+                <div>
+                <h3>Record Pointer or Relationship Marker field types with invalid record type(s) in constraint definitions</h3> 
+                </div> -->
+                <?php
+                    foreach ($dtysWithInvalidRectypeConstraint as $row) {
+                    ?>
+                    <div class="msgline"><b><?= $row['dty_Name'] ?></b> field (code <?= $row['dty_ID'] ?>) has 
+                        <?= count($row['invalidRectypeConstraint'])?> invalid record type constraint<?=(count($row['invalidRectypeConstraint'])>1?"s":"")?> 
+                        (code: <?= join(",",$row['invalidRectypeConstraint'])?>)
+                    </div>
 
-		</div>
-	</body>
+                    <?php
+                    }
+                }
+            ?>
+
+        </div>
+    </body>
 </html>
 
