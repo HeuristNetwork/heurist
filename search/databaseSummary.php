9916576d152611f4583326a7d5088d77737ab288
Jan Jaap de Groot <jjedegroot@gmail.com>
2014-08-19 12:33:55 +1000

    getRectypeRelationsAsXML has been created; copyrights added

--- search/databaseSummary.php
+++ search/databaseSummary.php
@@ -23,6 +23,7 @@
 * @author      Ian Johnson   <ian.johnson@sydney.edu.au>
 * @author      Stephen White   <stephen.white@sydney.edu.au>
 * @author      Artem Osmakov   <artem.osmakov@sydney.edu.au>
+* @author      Jan Jaap de Groot    <jjedegroot@gmail.com>
 * @copyright   (C) 2005-2013 University of Sydney
 * @link        http://Sydney.edu.au/Heurist
 * @version     3.1.0
