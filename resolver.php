a61e83b249323a5e3257f3cd68131085c8608384
Ian Johnson <ian.johnson@sydney.edu.au>
2015-06-23 14:11:48 +1000

    Move files from /h3 to root of h4

---
+++ resolver.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+* Copyright (C) 2005-2013 University of Sydney
+*
+* Licensed under the GNU License, Version 3.0 (the "License"); you may not use this file except
+* in compliance with the License. You may obtain a copy of the License at
+*
+* http://www.gnu.org/licenses/gpl-3.0.txt
+*
+* Unless required by applicable law or agreed to in writing, software distributed under the License
+* is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+* or implied. See the License for the specific language governing permissions and limitations under
+* the License.
+*/
+
+/**
+* resolver.php
+* acts as a PID redirector to view record  - provides a minimal URL for redirection
+*
+* @author      Ian Johnson   <ian.johnson@sydney.edu.au>
+* @author      Artem Osmakov   <artem.osmakov@sydney.edu.au>
+* @copyright   (C) 2005-2013 University of Sydney
+* @link        http://Sydney.edu.au/Heurist
+* @version     3.1.0
+* @license     http://www.gnu.org/licenses/gpl-3.0.txt GNU License 3.0
+* @package     Heurist academic knowledge management system
+* @subpackage  !!!subpackagename for file such as Administration, Search, Edit, Application, Library
+*/
+
+
+// called by applyCredentials require_once('configIni.php');
+// called by applyCredentials  require_once(dirname(__FILE__).'/common/config/initialise.php');
+require_once(dirname(__FILE__).'/h3/common/connect/applyCredentials.php');
+
+// Input is of the form .../resolver.php?db=mydatabase&recID=3456
+
+//$db = @$_REQUEST["db"];
+$id = @$_REQUEST["recID"];
+
+// Redirect to .../records/view/viewRecord.php
+
+header('Location: '.HEURIST_SITE_PATH.'/h3/records/view/viewRecord.php?db='.HEURIST_DBNAME.'&recID='.$id);
+
+return;
+
+?>
