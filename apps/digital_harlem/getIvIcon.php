e5d9d005ec108174789ecf40aa62e3ac48cf4b32
Artem Osmakov <osmakov@gmail.com>
2015-07-07 15:17:46 +1000

    icons on map

--- apps/digital_harlem/getIvIcon.php
+++
@@ -1,5 +0,0 @@
-<?php
-    header('Content-type: image/png');
-    $filelocation = "assets/assault.png";
-    readfile($filelocation);  
-?>
--- php/common/rt_icon.php
+++ php/common/rt_icon.php
@@ -27,9 +27,19 @@
     
     $system->initPathConstants($db);
     
-    if(substr($rectype_id,-4,4) != ".png") $rectype_id = $rectype_id . ".png";
+    $path = HEURIST_ICON_DIR; 
     
-    $filename = HEURIST_ICON_DIR . $rectype_id;
+    if(substr($rectype_id,-5,5) == "m.png") {
+        if(substr($rectype_id,0,4)=='term'){
+            $rectype_id = substr($rectype_id, 4);
+            $path = HEURIST_TERM_ICON_DIR;
+        }
+        $rectype_id = substr($rectype_id,0,-5) . ".png";   
+    }
+    else if(substr($rectype_id,-4,4) != ".png") $rectype_id = $rectype_id . ".png";
+    
+    
+    $filename = $path . $rectype_id;
     
     //
 
@@ -50,18 +60,40 @@
 
 function imagecopymerge_alpha($dst_im, $src_im, $dst_x, $dst_y, $src_x, $src_y, $src_w, $src_h, $pct){ 
     // creating a cut resource 
-    $cut = imagecreatetruecolor($src_w, $src_h); 
+    $cut = imagecreatetruecolor($src_w, $src_h);  //imagecreatetruecolor
 
     // copying relevant section from background to the cut resource 
     imagecopy($cut, $dst_im, 0, 0, $dst_x, $dst_y, $src_w, $src_h); 
 
     // copying relevant section from watermark to the cut resource 
     imagecopy($cut, $src_im, 0, 0, $src_x, $src_y, $src_w, $src_h); 
-
+    
     // insert cut resource to destination image 
     imagecopymerge($dst_im, $cut, $dst_x, $dst_y, 0, 0, $src_w, $src_h, $pct); 
-}     
-    
+}  
+
+/*
+        //set the image to 256 colours
+        imagetruecolortopalette($img_icon,0,256);
+        //Find the Chroma colour
+        $RemChroma = imagecolorexact( $img_icon,  0,0,255 );        
+        //Replace Chroma Colour
+        imagecolorset($img_icon, $RemChroma,0,0,254);
+        //Use function to convert back to true colour
+        imagepalettetotruecolor($img_icon);        
+
+function imagepalettetotruecolor(&$img)
+{
+        if (!imageistruecolor($img))
+        {
+            $w = imagesx($img);
+            $h = imagesy($img);
+            $img1 = imagecreatetruecolor($w,$h);
+            imagecopy($img1,$img,0,0,0,0,$w,$h);
+            $img = $img1;
+        }
+}   
+*/    
 //
 //
 //    
@@ -73,15 +105,19 @@ function create_rt_icon_with_bg( $rectype_id ){ //}, $bg_color ) {
     }else{
         $path = HEURIST_ICON_DIR; 
     }
+    
+    $alpha = 0;
 
     if(substr($rectype_id,-5,5) == "m.png") {
         $rectype_id = substr($rectype_id, 0, -5);
-        $bg_color = array(200,200,200);   //gray
+        $bg_color = array(190,228,248);  //array(200,200,200);   //gray
         $filename2 = $path . $rectype_id . "m.png";
+        $alpha = 40; //0-127
     }else if(substr($rectype_id,-5,5) == "s.png") {
         $rectype_id = substr($rectype_id, 0, -5);
         $bg_color = array(190,228,248);  //#bee4f8
         $filename2 = $path . $rectype_id . "s.png";
+        $alpha = 10;
     }else{
         return;
     }
@@ -93,8 +129,10 @@ function create_rt_icon_with_bg( $rectype_id ){ //}, $bg_color ) {
 //error_log($filename2);
     
     if(file_exists($filename)){
-        
-        $img = imagecreatetruecolor(25, 25);
+       
+        $img = imagecreatetruecolor(25, 25);      //truecolor
+        //imagealphablending($img, false);
+        //imagesavealpha($img, false);
         
         // fill the background color
         $bg = imagecolorallocate($img, 255, 127, 0);
@@ -105,14 +143,38 @@ function create_rt_icon_with_bg( $rectype_id ){ //}, $bg_color ) {
         imagefilledrectangle($img, 0, 0, 25, 25, $bg);
         
         // draw circle
+        //$col_ellipse = imagecolorallocatealpha($img, $bg_color[0], $bg_color[1], $bg_color[2], $alpha);
         $col_ellipse = imagecolorallocate($img, $bg_color[0], $bg_color[1], $bg_color[2]);
         imagefilledellipse($img, 12, 12 , 24, 24, $col_ellipse);        
         
         // load icon
         $img_icon = @imagecreatefrompng($filename);
         
+        //imagecopy($img, $img_icon, 4, 4, 0, 0, 16, 16); //keep bg of icon - transparent hole
+
         // merge icon
-        imagecopymerge_alpha($img, $img_icon, 4, 4, 0, 0, 16, 16, 75);
+        imagecopymerge_alpha($img, $img_icon, 4, 4, 0, 0, 16, 16, 120);  //mix background to dark
+        
+        
+        /*$bg = imagecolorallocate($img_icon, 255, 255, 255);
+        // make the background transparent
+        imagecolortransparent($img_icon, $bg);*/
+        
+        
+        /*$cut = imagecreatetruecolor(16, 16);  //imagecreatetruecolor
+        imagealphablending($cut, false);
+        //imagesavealpha($cut, true);
+        imagecopy($cut, $img_icon, 0, 0, 0, 0, 16, 16); 
+        imagecopymerge($img, $cut, 4, 4, 0, 0, 16, 16, 70);*/
+        
+        //imagealphablending($img_icon, true);
+        //imagesavealpha($img_icon, true);
+        //imagecopy($img, $img_icon, 4, 4, 0, 0, 16, 16); 
+        //imagecopymerge($img, $img_icon, 4, 4, 0, 0, 16, 16, 70); 
+        
+    //imagealphablending(, false);
+    
+        
         
         // output
         header("Content-type: image/png");
