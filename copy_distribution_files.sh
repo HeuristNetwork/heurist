4ac82e59707fafb57378467b4e429d2a329abb65
Ian Johnson <ian.johnson@sydney.edu.au>
2015-09-17 10:18:13 +1000

    Set up distribution scripts for h4

--- copy_distribution_files.sh
+++ copy_distribution_files.sh
@@ -1,13 +1,14 @@
 #! /bin/sh
 
-# copy_distribution_files.sh: Creates a distribution package in h3-build from a Heurist working directory
+# copy_distribution_files.sh: Creates a distribution package in h4-build from a Heurist working directory
+# This file is intended for internal use of the development team
 
 # @package     Heurist academic knowledge management system
 # @link        http://HeuristNetwork.org
 # @copyright   (C) 2005-2014 University of Sydney
 # @author      Ian Johnson     <ian.johnson@sydney.edu.au>
 # @license     http://www.gnu.org/licenses/gpl-3.0.txt GNU License 3.0
-# @version     3.2
+# @version     4.0
 
 # Licensed under the GNU License, Version 3.0 (the "License"); you may not use this file except in compliance
 # with the License. You may obtain a copy of the License at http://www.gnu.org/licenses/gpl-3.0.txt
@@ -16,72 +17,68 @@
 # See the License for the specific language governing permissions and limitations under the License.
 
 if [ -z $1 ]
-   then echo "Usage:   sudo ./copy_distribution_files.sh. h3.x.x.xxxx   - insert appropriate sub-versions and alpha or beta in x.x-xxxx"
+   then echo "Usage:   sudo ./copy_distribution_files.sh. h4.x.x.xxxx   - insert appropriate sub-versions and alpha or beta in x.x-xxxx"
    exit
    fi
 
 # copy_distribution_files.sh
 # --------------------------
 # This file copies all necessary Heurist vsn 3 distribution files and support files from the current directory
-# (any Heurist h3-xx program directory) to a temporary directory in the h3-build directory in /var/www/html/HEURIST.
+# (any Heurist h4-xx program directory) to a temporary directory in the h4-build directory in /var/www/html/HEURIST.
 # Make sure the current directory is the up-to-date version you want to package.
 # The script is set up for the HeuristScholar server and may require modification for other directory layouts
 
-# RUN THIS FILE FROM AN h3-xx DIRECTORY CONTAINING DESIRED HEURIST CODE
+# RUN THIS FILE FROM AN h4-xx DIRECTORY CONTAINING DESIRED HEURIST CODE
 
-# Creates h3-build directory in /var/www/html/HEURIST
+# Creates h4-build directory in /var/www/html/HEURIST
 
 
 echo -e "\n\n\n\n"
 echo -------------------------------------------------------------------------------------------------
 echo
-echo "Heurist Vsn 3 distribution builder - run from /var/www/html/HEURIST/h3-xx program directory"
+echo "Heurist Vsn 4 distribution builder - run from /var/www/html/HEURIST/h4-xx program directory"
 
-# File paths specified with explicit h3-build to ensure it will only remove from this specific directory
+# File paths specified with explicit h4-build to ensure it will only remove from this specific directory
 # and files in the subdirectory of this directory specified by the parameter
 
 # no effect if already exists
-mkdir /var/www/html/HEURIST/h3-build
+mkdir /var/www/html/HEURIST/h4-build
 
 echo
-echo removing existing h3-build/$1
-rm -rf /var/www/html/HEURIST/h3-build/$1
+echo removing existing h4-build/$1
+rm -rf /var/www/html/HEURIST/h4-build/$1
 
 echo
-echo copying files to /var/www/html/HEURIST/h3-build/$1
+echo copying files to /var/www/html/HEURIST/h4-build/$1
 
-mkdir /var/www/html/HEURIST/h3-build/$1
+mkdir /var/www/html/HEURIST/h4-build/$1
 
-# Copy all the files in the root of h3-xx
-cp -r *.* /var/www/html/HEURIST/h3-build/$1
+# Copy all the files in the root of h4-xx
+cp -r *.* /var/www/html/HEURIST/h4-build/$1
 
 # Remember to add any new directories here
-cp -r admin /var/www/html/HEURIST/h3-build/$1
-cp -r applications /var/www/html/HEURIST/h3-build/$1
-cp -r common /var/www/html/HEURIST/h3-build/$1
-cp -r context_help /var/www/html/HEURIST/h3-build/$1
-cp -r documentation /var/www/html/HEURIST/h3-build/$1
-cp -r export /var/www/html/HEURIST/h3-build/$1
-cp -r hapi /var/www/html/HEURIST/h3-build/$1
-cp -r import /var/www/html/HEURIST/h3-build/$1
-cp -r records /var/www/html/HEURIST/h3-build/$1
-cp -r search /var/www/html/HEURIST/h3-build/$1
-cp -r viewers /var/www/html/HEURIST/h3-build/$1
+cp -r apps /var/www/html/HEURIST/h4-build/$1
+cp -r assets /var/www/html/HEURIST/h4-build/$1
+cp -r help /var/www/html/HEURIST/h4-build/$1
+cp -r js /var/www/html/HEURIST/h4-build/$1
+cp -r migrated /var/www/html/HEURIST/h4-build/$1
+cp -r page /var/www/html/HEURIST/h4-build/$1
+cp -r php /var/www/html/HEURIST/h4-build/$1
 
 # remove any superfluous files - add others as appropriate
-rm -f /var/www/html/HEURIST/h3-build/$1/*.sh
+rm -f /var/www/html/HEURIST/h4-build/$1/*.sh
 
 # Now zip it all up as a tarball for distribution on the Heurist web site
 
 echo
-echo creating tarball /var/www/html/HEURIST/h3-build/$1.tar.bz2
-rm -f /var/www/html/HEURIST/h3-build/$1.tar.bz2
-tar -cjf /var/www/html/HEURIST/h3-build/$1.tar.bz2 -C /var/www/html/HEURIST/h3-build/ $1/
-rm -rf /var/www/html/HEURIST/h3-build/$1/
+echo creating tarball /var/www/html/HEURIST/h4-build/$1.tar.bz2
+rm -f /var/www/html/HEURIST/h4-build/$1.tar.bz2
+tar -cjf /var/www/html/HEURIST/h4-build/$1.tar.bz2 -C /var/www/html/HEURIST/h4-build/ $1/
+rm -rf /var/www/html/HEURIST/h4-build/$1/
 
 # show what we have got in the directory
 echo
-ls -alt /var/www/html/HEURIST/h3-build
+ls -alt /var/www/html/HEURIST/h4-build
 
 
 echo
@@ -105,7 +102,7 @@ echo ---------------------------------------------------------------------------
 echo
 echo "Now complete distribution (if code ready) with:"
 echo
-echo "    cp /var/www/html/HEURIST/h3-build/$1.tar.bz2  /var/www/html/HEURIST/DISTRIBUTION"
+echo "    cp /var/www/html/HEURIST/h4-build/$1.tar.bz2  /var/www/html/HEURIST/DISTRIBUTION"
 echo
 echo DO NOT CHANGE THE NAME of the tar.bz2 file - it extracts to a folder of this name
 echo and the installation is dependant on the filename parameter to find this folder
---
+++ migrated/xtras_utilities/verify_server.sh
@@ -0,0 +1,93 @@
+#! /bin/sh
+
+# verify_server.sh: Verifies packages etc. on Ubuntu/Debian server
+
+# Note: this file retrieved from old distribution on 16 sep 2015, may need updating
+
+# @package     Heurist academic knowledge management system
+# @link        http://HeuristNetwork.org
+# @copyright   (C) 2005-2014 University of Sydney
+# @author      Ian Johnson     <ian.johnson@sydney.edu.au>
+# @author      Brian Ballsun-Stanton <brian@fedarch.org>
+# @license     http://www.gnu.org/licenses/gpl-3.0.txt GNU License 3.0
+# @version     3.4
+
+# Licensed under the GNU License, Version 3.0 (the "License"); you may not use this file except in compliance
+# with the License. You may obtain a copy of the License at http://www.gnu.org/licenses/gpl-3.0.txt
+# Unless required by applicable law or agreed to in writing, software distributed under the License is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied
+# See the License for the specific language governing permissions and limitations under the License.
+
+echo -e "\n\n\n\n\n\n\n\n\n\n\n\n"
+
+echo
+echo "----------------------- Checking server for installed packages ---------------------------"
+echo
+echo
+
+
+echo -e "\n\n\n\n\n\n"
+
+echo "checking for: curl"
+if ! dpkg -s curl > /dev/null; then
+        echo "curl not found."
+        exit 1
+fi
+
+echo "checking for: wget"
+if ! dpkg -s wget > /dev/null; then
+        echo "wget not found."
+        exit 1
+fi
+
+echo "checking for: php5-curl"
+if ! dpkg -s php5-curl > /dev/null; then
+        echo "php5-curl not found."
+        exit 1
+fi
+
+echo "checking for: php5-xsl"
+if ! dpkg -s php5-xsl > /dev/null; then
+        echo "php5-xsl not found."
+        exit 1
+fi
+
+echo "checking for: php5-mysql"
+if ! dpkg -s php5-mysql > /dev/null; then
+        echo "php5-mysql not found."
+        exit 1
+fi
+
+echo "checking for: php5-memcached"
+if ! dpkg -s php5-memcached > /dev/null; then
+        echo "php5-memcached not found."
+        exit 1
+fi
+
+echo "checking for: memcached"
+if ! dpkg -s memcached > /dev/null; then
+        echo "memcached not found."
+        exit 1
+fi
+
+echo "checking for: php5-gd"
+if ! dpkg -s php5-gd > /dev/null; then
+        echo "php5-gd not found."
+        exit 1
+fi
+
+echo "checking for: libapache2-mod-php5"
+if ! dpkg -s libapache2-mod-php5 > /dev/null; then
+        echo "libapache2-mod-php5 not found."
+        exit 1
+
+echo -e "\n\n\n\n\n"
+echo
+echo "---- FINISHED -------------------------------------------"
+echo
+echo "If it gets to here, all expected packages are installed"
+echo
+echo
+echo
+echo
+echo
