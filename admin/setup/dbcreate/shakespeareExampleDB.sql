9c9745481bc15181a367b1f745de3870a5a6544d
Ian Johnson <ian.johnson@sydney.edu.au>
2016-02-24 13:38:06 +1100

    Set Vsn 4.2.2, addition of Shakespeare exemplar db, doco

--- admin/setup/dbcreate/shakespeareExampleDB.sql
+++
@@ -1,6 +0,0 @@
--- --------------------------------------------------------
-
--- This is a placeholder for a series of insert statements to populate the created database with Vincent's 'example data
-
--- --------------------------------------------------------
-
