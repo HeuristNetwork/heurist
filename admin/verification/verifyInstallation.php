233aca79c791578c38eb98f97c03f91341a0ca11
Ian Johnson <ian.johnson@sydney.edu.au>
2016-02-24 21:20:58 +1100

    Revised and more comprehensive verification of PHP extensions installed

--- admin/verification/verifyInstallation.php
+++ admin/verification/verifyInstallation.php
@@ -1,29 +1,29 @@
 <?php
 
-    /**
-    * verifyInstallation.php
-    * Verifies presence and correct versions for external JS components, Help, fiel directories and so forth
-    *
-    * @package     Heurist academic knowledge management system
-    * @link        http://HeuristNetwork.org
-    * @copyright   (C) 2005-2016 University of Sydney
-    * @author      Artem Osmakov   <artem.osmakov@sydney.edu.au>
-    * @author      Ian Johnson     <ian.johnson@sydney.edu.au>
-    * @license     http://www.gnu.org/licenses/gpl-3.0.txt GNU License 3.0
-    * @version     4.0
-    */
+/**
+* verifyInstallation.php
+* Verifies presence and correct versions for external JS components, Help, fiel directories and so forth
+*
+* @package     Heurist academic knowledge management system
+* @link        http://HeuristNetwork.org
+* @copyright   (C) 2005-2016 University of Sydney
+* @author      Artem Osmakov   <artem.osmakov@sydney.edu.au>
+* @author      Ian Johnson     <ian.johnson@sydney.edu.au>
+* @license     http://www.gnu.org/licenses/gpl-3.0.txt GNU License 3.0
+* @version     4.0
+*/
 
-    /*
-    * Licensed under the GNU License, Version 3.0 (the "License"); you may not use this file except in compliance
-    * with the License. You may obtain a copy of the License at http://www.gnu.org/licenses/gpl-3.0.txt
-    * Unless required by applicable law or agreed to in writing, software distributed under the License is
-    * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied
-    * See the License for the specific language governing permissions and limitations under the License.
-    */
+/*
+* Licensed under the GNU License, Version 3.0 (the "License"); you may not use this file except in compliance
+* with the License. You may obtain a copy of the License at http://www.gnu.org/licenses/gpl-3.0.txt
+* Unless required by applicable law or agreed to in writing, software distributed under the License is
+* distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied
+* See the License for the specific language governing permissions and limitations under the License.
+*/
 
-    // SHHOUKLDN'T NEED THESE, SHOUDL BE ABLE TO RUN STANDALONE
-    // require_once(dirname(__FILE__).'/../../common/connect/applyCredentials.php');
-    // require_once(dirname(__FILE__).'/../../common/php/dbMySqlWrappers.php');
+// SHHOUKLDN'T NEED THESE, SHOUDL BE ABLE TO RUN STANDALONE
+// require_once(dirname(__FILE__).'/../../common/connect/applyCredentials.php');
+// require_once(dirname(__FILE__).'/../../common/php/dbMySqlWrappers.php');
 
 ?>
 
@@ -68,35 +68,46 @@
         </div>
 
         <div id="page-inner">
-            This function verifies the presence of required program components
-            and the location and writability of folders required by the software.<br />
+            This function verifies the presence of required PHP extensions.
+            <!--and the location and writeability of folders required by the software.<br /> -->
             It assumes that all instances of Heurist are located in subdirectories
             in a common location (generally /var/www/html/HEURIST)<br />
             and point to the same Heurist filestore (generally .../HEURIST/HEURIST_FILESTORE,
             which is often a simlink to the real storage location).<br />
-            Checks include: external javascript functions; help system; exemplars; root file upload directory; index.html in parent. <br />
+            <!-- TODO: Checks should include: external javascript functions; help system; root file upload directory; index.html in parent. <br /> -->
 
             <h3>Please run this using the latest instance on your server</h3><br />
             If you use an older instance it may not pick up all requirements for the latest instance.<br/>&nbsp;<br/>
 
             <hr><br />
 
-            Verification under development Oct 2014 <br /><br />
             <?php
-                if (extension_loaded("curl")) {print "curl OK<br />";} else {print "curl MISSING<br />";}
-                if (extension_loaded("xsl")) {print "xsl OK<br />";} else {print "xsl MISSING<br />";}
-                if (extension_loaded("memcache")) {print "memcache OK<br />";} else {print "memcache MISSING<br />";}
-                if (extension_loaded("gd")) {print "gd OK<br />";} else {print "gd MISSING<br />";}
-                if (extension_loaded("zip")) {print "zip OK<br />";} else {print "zip MISSING<br />";}
-                // zip loads but unzip doesn't, but unzip is installed anyway (possibly with zip?)
-                // if (extension_loaded("unzip")) {print "unzip OK<br />";} else {print "unzip MISSING<br />";}
-                if (extension_loaded("mysql")) {print "mysql OK<br />";} else {print "mysql MISSING<br />";}
-                if (extension_loaded("pdo_mysql")) {print "pdo_mysql OK<br />";} else {print "pdo_mysql MISSING<br />";}
-                if (extension_loaded("pdo")) {print "pdo OK<br />";} else {print "pdo MISSING<br />";}
-                if (extension_loaded("mbstring")) {print "mbstring OK<br />";} else {print "mbstring MISSING<br />";}
+            if (extension_loaded("curl")) {print "curl ok<br />";} else {print "curl MISSING<br />";}
+            if (extension_loaded("xsl")) {print "xsl ok<br />";} else {print "xsl MISSING<br />";}
+            if (extension_loaded("mysql")) {print "mysql ok<br />";} else {print "mysql MISSING<br />";}
+            if (extension_loaded("mysqli")) {print "mysqli ok<br />";} else {print "mysqli MISSING<br />";}
+            if (extension_loaded("memcache")) {print "memcache ok<br />";} else {print "memcache MISSING<br />";}
+            if (extension_loaded("SPL")) {print "SPL ok<br />";} else {print "SPL MISSING<br />";}
+            if (extension_loaded("filter")) {print "filter ok<br />";} else {print "filter MISSING<br />";}
+            if (extension_loaded("pcre")) {print "pcre ok<br />";} else {print "pcre MISSING<br />";}
+            if (extension_loaded("xml")) {print "xml ok<br />";} else {print "xml MISSING<br />";}
+            if (extension_loaded("simpleXML")) {print "simpleXML ok<br />";} else {print "simpleXML MISSING<br />";}
+            if (extension_loaded("mbstring")) {print "mbstring ok<br />";} else {print "mbstring MISSING<br />";}
+            if (extension_loaded("json")) {print "json ok<br />";} else {print "json MISSING<br />";}
+            if (extension_loaded("session")) {print "session ok<br />";} else {print "session MISSING<br />";}
+            if (extension_loaded("dom")) {print "dom ok<br />";} else {print "dom MISSING<br />";}
+            if (extension_loaded("gd")) {print "gd ok<br />";} else {print "gd MISSING<br />";}
+            if (extension_loaded("zip")) {print "zip ok<br />";} else {print "zip MISSING<br />";}
+            // zip loads but unzip doesn't, but unzip is installed anyway (possibly with zip?)
+            // if (extension_loaded("unzip")) {print "unzip ok<br />";} else {print "unzip MISSING<br />";}
 
-                print "<h3>All loaded extensions:</h3><br />";
-                print_r(get_loaded_extensions());
+            if (extension_loaded("pdo")) {print "pdo ok<br />";} else {print "pdo MISSING, optional, required for FAIMA<br />";}
+            if (extension_loaded("pdo_sqlite")) {print "pdo_sqlite ok<br />";} else {print "pdo_sqlite MISSING, optional, required for FAIMA<br />";}
+            if (extension_loaded("exif")) {print "exif ok<br />";} else {print "exif MISSING, optional, required for image file indexing<br />";}
+
+            print "<h3>All loaded extensions:</h3><br />";
+            // TODO: write lsit of loaded extensions out neatly rather than dumping the array
+            print_r(get_loaded_extensions());
             ?>
 
             <br /><hr><br />
