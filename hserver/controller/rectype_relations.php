89fc758f96631bcdd313ae15b77e557a633e5831
Artem Osmakov <osmakov@gmail.com>
2016-03-29 17:39:53 +0300

    db summary:  count relmarkers

--- hserver/controller/rectype_relations.php
+++ hserver/controller/rectype_relations.php
@@ -88,7 +88,12 @@
         $relations = array();
         
         // Select all relation details that have "dty_PtrTargetRectypeIDs" defined.  The defRecStructure table is used to determine the structure of a record. The defDetailTypes and recDetails tabes are used to ultimately get access to the "dty.dty_PtrTargetRectypeIDs" field. This field stores a comma seperated links of record types where this record points to. 
-        $query = "SELECT rst_DetailTypeID as id, rst_DisplayName as name, COUNT(rd.dtl_ID) as count, dty.dty_Type as reltype, dty.dty_PtrTargetRectypeIDs as ids FROM defRecStructure rst INNER JOIN defDetailTypes dty ON rst.rst_DetailTypeID=dty.dty_ID LEFT JOIN recDetails rd ON rd.dtl_DetailTypeID=rst.rst_DetailTypeID WHERE rst.rst_RectypeID=" .$rectype->id. " AND NOT (dty.dty_PtrTargetRectypeIDs IS NULL OR dty.dty_PtrTargetRectypeIDs='') GROUP BY rst.rst_DetailTypeID;";
+        $query = "SELECT rst_DetailTypeID as id, rst_DisplayName as name, COUNT(rd.dtl_ID) as count, dty.dty_Type as reltype, "
+        ."dty.dty_PtrTargetRectypeIDs as ids FROM defRecStructure rst INNER JOIN defDetailTypes dty ON rst.rst_DetailTypeID=dty.dty_ID "
+        ."LEFT JOIN recDetails rd ON rd.dtl_DetailTypeID=rst.rst_DetailTypeID "
+        ."WHERE rst.rst_RectypeID=" .$rectype->id. " AND NOT (dty.dty_PtrTargetRectypeIDs IS NULL OR dty.dty_PtrTargetRectypeIDs='') "
+        ."GROUP BY rst.rst_DetailTypeID;";
+        
         $res = $system->get_mysqli()->query($query);
         while($row = $res->fetch_assoc()) { 
             $relation = new stdClass();
@@ -117,10 +122,24 @@
         
         // Go through all ID's
         //echo "\nID's for relation #" . $relation->id . ": " . $relation->ids;
-        $ids = explode(",", $relation->ids);
+        $ids = explode(",", $relation->ids); //targer record types ids
         foreach($ids as $id) {
-            // Count how many times the $relation points to this id. The recLinks table is used to determine record links. rl_sourceID and rl_targetID details are looked up in the Records table. Actual relationships are then filtered by using the record type id and detail type id of $relation and the record type id of the target record.   
-            $query = "SELECT COUNT(r2.rec_ID) as count, rl.rl_DetailTypeID, r1.rec_Title, r1.rec_RecTypeID, r2.rec_Title, r2.rec_RecTypeID FROM recLinks rl INNER JOIN Records r1 ON r1.rec_ID=rl.rl_SourceID INNER JOIN Records r2 ON r2.rec_ID=rl.rl_TargetID WHERE rl.rl_DetailTypeID=" .$relation->id. " AND r1.rec_RecTypeID=" .$rectype->id. " AND r2.rec_RecTypeID=".$id;
+            if($relation->type=='relmarker'){
+                
+                $query = "SELECT COUNT(r2.rec_ID) as count, rl.rl_DetailTypeID, r1.rec_Title, r1.rec_RecTypeID, r2.rec_Title, r2.rec_RecTypeID "
+                ."FROM recLinks rl INNER JOIN Records r1 ON r1.rec_ID=rl.rl_SourceID "
+                ."INNER JOIN Records r2 ON r2.rec_ID=rl.rl_TargetID "
+                ."WHERE rl.rl_DetailTypeID IS NULL AND r1.rec_RecTypeID=" .$rectype->id. " AND r2.rec_RecTypeID=".$id;
+            }else{
+                // Count how many times the $relation points to this id. The recLinks table is used to determine record links. 
+                // rl_sourceID and rl_targetID details are looked up in the Records table. 
+                // Actual relationships are then filtered by using the record type id and detail type id of $relation and 
+                // the record type id of the target record.   
+                $query = "SELECT COUNT(r2.rec_ID) as count, rl.rl_DetailTypeID, r1.rec_Title, r1.rec_RecTypeID, r2.rec_Title, r2.rec_RecTypeID "
+                ."FROM recLinks rl INNER JOIN Records r1 ON r1.rec_ID=rl.rl_SourceID "
+                ."INNER JOIN Records r2 ON r2.rec_ID=rl.rl_TargetID "
+                ."WHERE rl.rl_DetailTypeID=" .$relation->id. " AND r1.rec_RecTypeID=" .$rectype->id. " AND r2.rec_RecTypeID=".$id;
+            }
            
             if($res = $system->get_mysqli()->query($query)) {
                 if($row = $res->fetch_assoc()) {
@@ -169,12 +188,23 @@
 
             // Find all targets for each relation
             foreach($relations as $relation) {
+                //get counts by target
                 $targets = getTargets($system, $rectypes[$i], $relation);
                 
                 // Construct a link for each target
                 foreach($targets as $target) {
                     $link = new stdClass();
-                    
+/* example                    
+relation:Object
+    count:0
+    id:16
+    ids:"10"
+    name:"Person(s) concerned"
+    type:"resource"
+source:1
+target:9
+targetcount:0                    
+*/
                     // Records
                     $link->source = $i;
                     $link->target = getIndex($rectypes, $target);
