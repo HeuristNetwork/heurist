a7f6c6796bc73f1469fd674c79a9120e3a966393
ijohnson222 <ian.johnson@sydney.edu.au>
2016-08-25 11:01:55 +1000

    Vsn 4.2.10.beta distribution

--- configIni.php
+++ configIni.php
@@ -62,8 +62,9 @@ of the codebase and rename it to index.html
 
 // *** DO NOT CHANGE VERSION NUMBER, THIS IS SET BY THE HEURIST DEVELOPMENT TEAM ***
 
-$version = "4.2.9"; // sets current program version number, determined by Heurist development lead
+$version = "4.2.10"; // sets current program version number, determined by Heurist development lead
 
+// Vsn 4.2.10 22 Aug 2016  Update distribution on Heurist server
 // Vsn 4.2.9 11 Aug 2016  First fully working draft of new importer
 // Vsn 4.2.8 17 Jun 2016  Further small fixes 
 // Vsn 4.2.7 14 May 2016  Further small fixes 
