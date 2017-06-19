43f8bbc9927ef2bd86aa7316ac16a85258ecb148
ijohnson222 <ian.johnson@sydney.edu.au>
2017-06-19 11:35:09 +0200

    Document Digital Harlem analytics codes

--- hclient/widgets/digital_harlem/google_analytics.js
+++ hclient/widgets/digital_harlem/google_analytics.js
@@ -1,7 +1,7 @@
   // Google Analytics for Digital Harlem
   var _gaq = _gaq || [];
-//_gaq.push(['_setAccount', 'UA-22188173-1']);
-  _gaq.push(['_setAccount', 'UA-50291815-1']);
+//_gaq.push(['_setAccount', 'UA-22188173-1']);  // Original Digital Harlem analytics account owned by Ian Johnson
+  _gaq.push(['_setAccount', 'UA-50291815-1']);  // New Digital Harlem analytics account owned by Stephen Robertson
   _gaq.push(['_trackPageview']);
 
   (function() {
