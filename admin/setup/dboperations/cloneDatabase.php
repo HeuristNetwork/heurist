c8155adab927fb7176293fe7046956660aa0ae67
Artem Osmakov <osmakov@gmail.com>
2018-07-18 11:47:46 +0400

    clone database - fix copy files for templates

--- admin/setup/dboperations/cloneDatabase.php
+++ admin/setup/dboperations/cloneDatabase.php
@@ -423,7 +423,13 @@ function cloneDatabase($targetdbname, $nodata=false, $templateddb, $user_id) {
 
     // Copy the images and the icons directories
     //TODO: Needs error report, trap error and warn or abort clone
-    recurse_copy( HEURIST_UPLOAD_ROOT.HEURIST_DBNAME, HEURIST_UPLOAD_ROOT.$targetdbname );
+    if(strpos($source_database,'hdb_')!==0){
+        $source_database_short = $source_database;
+    }else{
+        $source_database_short = substr($source_database, 4);
+    }
+    
+    recurse_copy( HEURIST_UPLOAD_ROOT.$source_database_short, HEURIST_UPLOAD_ROOT.$targetdbname );
 
     // Update file path in target database  with absolute paths
     $query1 = "update recUploadedFiles set ulf_FilePath='".HEURIST_UPLOAD_ROOT.$targetdbname.
