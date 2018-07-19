b1f98bc1c023c0cbdd88900ac2c288a97ca62acf
Claire Reeler <claire@archefact.com.au>
2018-07-19 11:46:30 +1000

    More typos fixed.

--- admin/structure/rectypes/manageRectypes.js
+++ admin/structure/rectypes/manageRectypes.js
@@ -757,7 +757,7 @@ function RectypeManager() {
                 'definitions from templates (Heurist databases registered in the Heurist clearinghouse). '+
                 'Those with registration IDs less than 1000 are templates curated by the Heurist team. '
                 +'\n\n'
-+'This is particularly important for BIBLIOGRAPHIC record types - the definitions in template #6 (Bibliographic definitions) are' 
++'This is particularly important for BIBLIOGRAPHIC record types - the definitions in template #6 (Bibliographic definitions) are ' 
 +'optimally normalised and ensure compatibility with bibliographic functions such as Zotero synchronisation, Harvard format and inter-database compatibility.'                
                 
                 +'\n\nUse:  Manage tab > Structure > Browse templates')){
