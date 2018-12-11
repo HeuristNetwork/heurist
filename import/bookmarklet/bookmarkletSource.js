3bf416924c6ee3c5c328e5e62657134aa8897f32
ijohnson222 <ian.johnson@sydney.edu.au>
2018-12-11 12:59:44 +1100

    Get rid of only active reference to h4

--- import/bookmarklet/bookmarkletSource.js
+++ import/bookmarklet/bookmarkletSource.js
@@ -20,4 +20,4 @@
 * See the License for the specific language governing permissions and limitations under the License.
 */
 
-javascript:(function(){h='http://heurist.sydney.edu.au/h4/';d=document;c=d.contentType;if(c=='text/html'||!c){if(d.getElementById('__heurist_bookmarklet_div'))return Heurist.init();s=d.createElement('script');s.type='text/javascript';s.src=(h+'import/bookmarklet/bookmarkletPopup.php?'+new Date().getTime()).slice(0,-8);d.getElementsByTagName('head')[0].appendChild(s);}else{e=encodeURIComponent;w=open(h+'records/add/addRecordPopup.php?t='+e(d.title)+'&u='+e(location.href));window.setTimeout('w.focus()',200);}})();
+javascript:(function(){h='http://heurist.sydney.edu.au/heurist/';d=document;c=d.contentType;if(c=='text/html'||!c){if(d.getElementById('__heurist_bookmarklet_div'))return Heurist.init();s=d.createElement('script');s.type='text/javascript';s.src=(h+'import/bookmarklet/bookmarkletPopup.php?'+new Date().getTime()).slice(0,-8);d.getElementsByTagName('head')[0].appendChild(s);}else{e=encodeURIComponent;w=open(h+'records/add/addRecordPopup.php?t='+e(d.title)+'&u='+e(location.href));window.setTimeout('w.focus()',200);}})();
