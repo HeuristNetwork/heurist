30bfdd987727e5b7f382aea6fdd6d0c5865b9776
ijohnson222 <ian.johnson@sydney.edu.au>
2018-12-15 13:52:19 +1100

    Version 5.0.4 distribution

--- configIni.php
+++ configIni.php
@@ -62,8 +62,9 @@ of the codebase and rename it to index.html
 
 // *** DO NOT CHANGE VERSION NUMBER, THIS IS SET BY THE HEURIST DEVELOPMENT TEAM ***
 
-$version = "5.0.3"; // sets current program version number, determined by Heurist development lead
+$version = "5.0.4"; // sets current program version number, determined by Heurist development lead
 
+// h5.0.4 Minor fixes, CSV output, dashbpard creation, icon directories etc.
 // 5.0.3 11 Dec 2018 WYSIWYG edito for memos, extract text from PDFs, improve verification and child record conversion, fix thumbnail and bookmarklet services, manually sorted result sets, improved ViewRecord, entity types with counts shortcut, filtering menus by user level, fixes to CSV export, finalise Beyond 1914 and Adelaide websites
 // 5.0.2 10 Nov 2018 Really working h5. Dashboard, streamlined field definition, read-only groups, revised import templates, conversion to child records, U of Adelaide website from Expert nation, finalise Beyond1914.
 // 5.0.1 25 Jul 18  1)php7 conformity 2)got rid h3 client libs 3)got rid mysql__ functions
