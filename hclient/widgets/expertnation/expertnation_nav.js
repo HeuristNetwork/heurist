38a55965a2f2352a9eb6edff1456f816805c57f3
Artem Osmakov <osmakov@gmail.com>
2019-02-28 12:08:53 +0400

    heurist and heuristplus in EN

--- hclient/widgets/expertnation/expertnation_nav.js
+++ hclient/widgets/expertnation/expertnation_nav.js
@@ -265,8 +265,8 @@ $.widget( "heurist.expertnation_nav", {
                 //add section on page for database content
                 var ele = that._addClearPageDiv(recID);                
 
-                //for map.php witnin iframe
-                content = content.replace('https://heuristplus.sydney.edu.au/heurist/',window.hWin.HAPI4.baseURL);
+                //replace reference in webcontent (for map.php witnin iframe) to new server
+                content = content.replace('http://heurist.sydney.edu.au/h5-ao/',window.hWin.HAPI4.baseURL);
 
                 //add page content from database  
                 ele.html( content );    
