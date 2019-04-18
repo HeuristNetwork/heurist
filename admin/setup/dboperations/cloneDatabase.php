f75d0357fd1923d7a071f38c2d4135faafb5baee
Artem Osmakov <osmakov@gmail.com>
2019-04-18 13:15:11 +0400

    Clone db: dont copy files for clone structure

--- admin/setup/dboperations/cloneDatabase.php
+++ admin/setup/dboperations/cloneDatabase.php
@@ -419,7 +419,7 @@ function cloneDatabase($targetdbname, $nodata=false, $templateddb, $user_id) {
 
     // Copy the images and the icons directories
     //TODO: Needs error report, trap error and warn or abort clone
-
+    if(!$nodata)
     folderRecurseCopy( HEURIST_FILESTORE_ROOT.$source_database, HEURIST_FILESTORE_ROOT.$targetdbname );
 
     // Update file path in target database  with absolute paths
