9d8d65d46c3d0c333225aab1104520b5e4fa6739
Artem Osmakov <osmakov@gmail.com>
2020-06-13 11:57:01 +0400

    Bugfix for default rst_Status

--- hsapi/entity/dbDefRecStructure.php
+++ hsapi/entity/dbDefRecStructure.php
@@ -213,8 +213,8 @@ class DbDefRecStructure extends DbEntityBase
             }else{
                 $this->records[$idx]['rst_ID'] = $row['rst_ID'];
                 $this->records[$idx]['rst_LocallyModified'] = ($row['rst_OriginatingDBID']>0)?1:0;
+                if(@$this->records[$idx]['rst_Status']=='') $this->records[$idx]['rst_Status'] = 'open';
             }
-
 
             if(@$this->records[$idx]['rst_MaxValues']==null ||
                 !(intval(@$this->records[$idx]['rst_MaxValues'])>=0)) $this->records[$idx]['rst_MaxValues'] = 1;
