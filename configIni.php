dd4ae46fd9e08a0f697849722b940d39118d9a5c
ianjohnson <ian.johnson@sydney.edu.au>
2020-08-25 15:39:28 +0200

    Version 5.2.13 distribution

--- configIni.php
+++ configIni.php
@@ -62,10 +62,13 @@ of the codebase and rename it to index.html
 
 // *** DO NOT CHANGE VERSION NUMBER, THIS IS SET BY THE HEURIST DEVELOPMENT TEAM ***
 
-$version = "5.2.12"; // sets current program version number, determined by Heurist development lead
+$version = "5.2.13"; // sets current program version number, determined by Heurist development lead
 
 // dates below are the date of release of the given version, and list changes since last release
 
+// dates below are the date of release of the given version, and list changes since last release
+
+// h5.2.13 25 Aug 2020 Improve speed on record view, fix problems with loading xml and with web site functions, new USyd server, Initial development of H6 interface in h6dev branch  
 // h5.2.12 03 Aug 2020 Initial work on version 6 interface, new menu strucutre, design, import, explore, admin mostly working.
 // h5.2.11 12 July 2020 Sort pointer field alphabetically, table view and data table widget
 // h5.2.10 5 July 2020 Consistent ordering of record types in trees, fix crosstabs, convert UTM gridref coords in polygons, first draft data table view in main interface and website widget
