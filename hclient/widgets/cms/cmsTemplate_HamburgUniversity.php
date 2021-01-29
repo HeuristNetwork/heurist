8d495234aa74b9cfdcff46d8c090e546caa3333a
Artem Osmakov <osmakov@gmail.com>
2021-01-29 20:07:21 +0400

    Correct name for script and style file

--- hclient/widgets/cms/cmsTemplate_HamburgUniversity.php
+++ hclient/widgets/cms/cmsTemplate_HamburgUniversity.php
@@ -29,7 +29,8 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
     <?php
-        include 'heurist_scripts_and_styles.php'  
+        // add heurist/hclient/widgets/cms/ if you modify this file and copy to heurist root folder
+        include 'websiteScriptAndStyles.php';  
     ?>
     <style>
         body{
