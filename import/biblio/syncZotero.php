844a83ab763e5dda4126eaa7c5f8d4354aba172d
Artem Osmakov <osmakov@gmail.com>
2021-02-10 19:48:07 +0400

    scroll zotero sync panel

--- import/biblio/syncZotero.php
+++ import/biblio/syncZotero.php
@@ -119,7 +119,7 @@ if($fh_data==null || is_string($fh_data)){
         </script>
     </head>
 
-    <body class="popup" style="margin-right:30px">
+    <body class="popup" style="margin-right:30px;overflow:auto">
 
         <?php
         
