e1b773c918dce59771959b75cf59bba59d375a35
Artem Osmakov <osmakov@gmail.com>
2021-04-02 20:02:30 +0400

    search builder: field tree cosmetics

--- hclient/core/utils_dbs.js
+++ hclient/core/utils_dbs.js
@@ -303,9 +303,9 @@ window.hWin.HEURIST4.dbs = {
                 if(all_header_fields){
                     var s = '<span style="font-style:italic">Generic fields</span>';
                     $children = [
-                        {title:s, folder:true, children:$children},
-                        {key:'anyfield', type:'freetext',
-                        title:"Any field", 
+                        {title:s, folder:true, is_generic_fields:true, children:$children},
+                        {key:'anyfield', type:'freetext',                       //-16px -80px      -48px -80px open
+                        title:"<span style='font-size:0.9em;font-style:italic;padding-left:22px'>ANY FIELD</span>", 
                         code:($recTypeId+':anyfield'), name:'Any field'}
                         ];
                 }
