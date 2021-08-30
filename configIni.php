aa09ef303149734493f53beb98bebee8addb66a9
ianjohnson <ian.johnson@sydney.edu.au>
2021-08-30 11:14:15 +0200

    Version 6.2.17 distribution

--- configIni.php
+++ configIni.php
@@ -62,11 +62,12 @@ of the codebase and rename it to index.html
 
 // *** DO NOT CHANGE VERSION NUMBER, THIS IS SET BY THE HEURIST DEVELOPMENT TEAM ***
 
-$version = "6.2.16"; // sets current program version number, determined by Heurist development lead
+$version = "6.2.17"; // sets current program version number, determined by Heurist development lead
 
 // dates below are the date of release of the given version, and list changes since last release
 
-// 6.2.15&16 Ability to change filestore path not jsut via simlinks, web site editing and multibyute character fixes, final removal of yui code and move other old code to /shelved, improve some scary messages, standard check and download of missing record type, imporved file uploads and recognition incl. PDFs, bigger tabs on visualisation panel, improved Map tab - controls at top, turn map and timeline on separately, new functiosn for saving CSVs
+// 6.2.17 30 Aug 2021 Uploading of tile stack images, convert 3-34 to from Text field to File field, added supplementart title 2 for websites, first draft bulk emailer
+// 6.2.15&16 20 Aug 2021 Ability to change filestore path not jsut via simlinks, web site editing and multibyute character fixes, final removal of yui code and move other old code to /shelved, improve some scary messages, standard check and download of missing record type, imporved file uploads and recognition incl. PDFs, bigger tabs on visualisation panel, improved Map tab - controls at top, turn map and timeline on separately, new functiosn for saving CSVs
 // 6.2.14 =8 July 2021 Improivements to field selection trees in filters - additional record-level fields, layout and memo texts, better layout of rule builder dropdowns and auto selection of start point, fix html escapes in Gephi output, match missing fields correctly in csv import
 // 6.2.13 26 June 2021 Multiple allocation of lookup services to different recor types, fix for zero height div in website
 // 6.2.12 22 June 2021 Add blog to new websites, new Zotero mapping, improve configuration of external lookup services, elimination of the use of yui, 
