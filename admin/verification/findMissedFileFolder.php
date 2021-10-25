64501f7c6e4ce6ef5376fae2397ba8cf4fff4308
Artem Osmakov <osmakov@gmail.com>
2021-10-25 07:47:32 +0400

    Find missed and not writeable folders for all databases

--- admin/verification/findMissedFileFolder.php
+++ admin/verification/findMissedFileFolder.php
@@ -1,86 +1,190 @@
 <?php
+    /*
+    * Copyright (C) 2005-2020 University of Sydney
+    *
+    * Licensed under the GNU License, Version 3.0 (the "License"); you may not use this file except
+    * in compliance with the License. You may obtain a copy of the License at
+    *
+    * http://www.gnu.org/licenses/gpl-3.0.txt
+    *
+    * Unless required by applicable law or agreed to in writing, software distributed under the License
+    * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+    * or implied. See the License for the specific language governing permissions and limitations under
+    * the License.
+    */
 
-/**
-* findMissedFileFolder - list of all databases w/o upload folder
-*
-* @package     Heurist academic knowledge management system
-* @link        http://HeuristNetwork.org
-* @copyright   (C) 2005-2020 University of Sydney
-* @author      Tom Murtagh
-* @author      Kim Jackson
-* @author      Artem Osmakov   <artem.osmakov@sydney.edu.au>
-* @author      Ian Johnson     <ian.johnson@sydney.edu.au>
-* @license     http://www.gnu.org/licenses/gpl-3.0.txt GNU License 3.0
-* @version     3.1.0
-*/
+    /**
+    * Find missed system folders for all databases
+    *
+    * @author      Artem Osmakov   <artem.osmakov@sydney.edu.au>
+    * @copyright   (C) 2005-2020 University of Sydney
+    * @link        http://HeuristNetwork.org
+    * @version     3.1
+    * @license     http://www.gnu.org/licenses/gpl-3.0.txt GNU License 3.0
+    * @package     Heurist academic knowledge management system
+    * @subpackage  !!!subpackagename for file such as Administration, Search, Edit, Application, Library
+    */
 
-/*
-* Licensed under the GNU License, Version 3.0 (the "License"); you may not use this file except in compliance
-* with the License. You may obtain a copy of the License at http://www.gnu.org/licenses/gpl-3.0.txt
-* Unless required by applicable law or agreed to in writing, software distributed under the License is
-* distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied
-* See the License for the specific language governing permissions and limitations under the License.
-*/
 ini_set('max_execution_time', 0);
-
-//define('OWNER_REQUIRED',1);   
+ 
 define('PDIR','../../');  //need for proper path to js and css    
 
 require_once(dirname(__FILE__).'/../../hclient/framecontent/initPageMin.php');
 
-    $mysqli = $system->get_mysqli();
+/*
+if( $system->verifyActionPassword($_REQUEST['pwd'], $passwordForServerFunctions) ){
+    ?>
     
-    $counter = 0;
-            
-    $dbs = mysql__getdatabases4($mysqli, true);   
-    $inaccessible = array();
-    $missed = array();
-             
-    foreach ($dbs as $db){
-        
-        //if($counter>50) break;
-        $counter++;
-        
-        $dbName = substr($db,4);
-        $folder = HEURIST_FILESTORE_ROOT . $dbName . '/';
-        if(file_exists($folder) && is_dir($folder)){
-            if (!is_writable($folder)) {
-                array_push($inaccessible, $db);    
-            }
-        }else{
-            array_push($missed, $db);
-        }
-    }//for
+    <form action="verifyConceptCodes4.php" method="POST">
+        <div style="padding:20px 0px">
+            Only an administrator (server manager) can carry out this action.<br />
+            This action requires a special system administrator password (not a normal login password)
+        </div>
+    
+        <span style="display: inline-block;padding: 10px 0px;">Enter password:&nbsp;</span>
+        <input type="password" name="pwd" autocomplete="off" />
 
-    if(count($missed)>0 || count($inaccessible)>0){
-        $rep = '<h2>List of databases with missing/inaccessible upload folder</h2><hr>';
-        if(count($missed)>0){
-            $rep .= '<h3>Missed</h3>';
-            foreach ($missed as $db){
-                $rep .= ($db.'<br>');
-            }
-        }    
-        if(count($inaccessible)>0){
-            $rep .= '<h3>Inaccessible</h3>';
-            foreach ($inaccessible as $db){
-                $rep .= ($db.'<br>');
-            }
-        }    
-    }
-    if(@$_REQUEST['mail']){
-        
-        $email_header = " <no-reply@".HEURIST_SERVER_NAME.">\r\nContent-Type: text/html;";
-        $rv = sendEmail(HEURIST_MAIL_TO_ADMIN, 'List of databases with missing/inaccessible upload folder', 
-                                            $rep, $email_header);
-        exit();
-    }
-?>
+        <input type="submit" value="OK" />
+    </form>
+
+    <?php
+    exit();
+}
+*/
+if(!@$_REQUEST['mail']){
+?>            
 <html>
     <head>
         <meta http-equiv="content-type" content="text/html; charset=utf-8">
         <link rel="stylesheet" type="text/css" href="<?php echo PDIR;?>h4styles.css" />
     </head>
     <body class="popup">
-        <?php print $rep; ?>
-    </body>
-</html>
+
+        <script>window.history.pushState({}, '', '<?php echo $_SERVER['PHP_SELF']; ?>')</script>  
+        <div style="font-family:Arial,Helvetica;font-size:12px">
+                <h2>List of databases with missed or not-writeable folders</h2>
+<?php            
+}
+
+    $mysqli = $system->get_mysqli();
+    
+    //1. find all database
+    $databases = mysql__getdatabases4($mysqli, false);   
+
+/*    
+    $query = 'show databases';
+    $res = $mysqli->query($query);
+    if (!$res) {  print $query.'  '.$mysqli->error;  return; }
+    $databases = array();
+    while (($row = $res->fetch_row())) {
+        if( strpos($row[0], 'hdb_')===0 ){
+            //if($row[0]>'hdb_Masterclass_Cookbook')
+                $databases[] = $row[0];
+        }
+    }
+*/
+
+    $root = $system->getFileStoreRootFolder();
+    
+    $check_subfolders = true;
+    
+    $folders = $system->getArrayOfSystemFolders();
+      
+    $not_exists = array();
+    $not_writeable = array();
+    $not_exists2 = array();
+    $not_writeable2 = array();
+    
+    foreach ($databases as $idx=>$db_name){
+
+        //list($database_name_full, $db_name) = mysql__get_names($db_name);
+            
+        $dir = $root.$db_name.'/';
+        
+        $check = folderExists($dir, true);
+        if($check==-1){
+            $not_exists[] = $db_name;
+        }else if($check<0){    
+            $not_writeable[] = $db_name;
+        }else if($check_subfolders){
+             //check subfolders
+             foreach ($folders as $folder_name=>$folder){
+                 
+                 if($folder[0]=='' || $folder[0]==null || $folder_name=='uploaded_tilestacks') continue;
+                 
+                 $subdir = $dir.$folder_name.'/';
+                 $check = folderExists($subdir, true);
+                 if($check==-1){
+                      $not_exists2[] = $subdir;
+                 }else if($check<0){    
+                      $not_writeable2[] = $subdir;
+                 }
+             }
+             
+        }
+            
+    }//while  databases
+    
+    $rep = '';
+
+    if(count($not_exists)>0){
+        $rep.='<h3>MISSED HEURIST_FILESTORE_DIR for databases:</h3>';
+        
+        foreach ($not_exists as $db_name){
+                $rep.=$db_name.'<br>';
+        }
+        $rep.='<hr>';
+    }
+    if(count($not_writeable)>0){
+        $rep.='<h3>NOT WRITEABLE HEURIST_FILESTORE_DIR for databases:</h3>';
+        
+        foreach ($not_writeable as $db_name){
+                $rep.=$db_name.'<br>';
+        }
+        $rep.='<hr>';
+    }
+
+    if(count($not_exists2)>0){
+        $rep.='<h3>MISSED subfolders</h3>';
+        
+        foreach ($not_exists2 as $dir){
+            if(strpos($dir,'file_uploads')>0){
+                $rep.='<b>'.$dir.'</b><br>';
+            }else{
+                $rep.=$dir.'<br>';
+            }
+        }
+        $rep.='<hr>';
+    }
+    if(count($not_writeable2)>0){
+        $rep.='<h3>NOT WRITEABLE subfolders</h3>';
+        
+        foreach ($not_writeable2 as $dir){
+            if(strpos($dir,'file_uploads')>0){
+                $rep.='<b>'.$dir.'</b><br>';
+            }else{
+                $rep.=$dir.'<br>';
+            }
+        }
+        $rep.='<hr>';
+    }
+
+    
+    $rep.='[end report]</div>';
+    
+    if(@$_REQUEST['mail']){
+        
+        $email_header = " <no-reply@".HEURIST_SERVER_NAME.">\r\nContent-Type: text/html;";
+        //
+        $rv = sendEmail(HEURIST_MAIL_TO_ADMIN, 'List of databases with missing/inaccessible folders', 
+                                            $rep, $email_header);
+        exit();
+    }else{
+        
+        print $rep;
+        print '</body></html>';
+
+    }
+    
+   
+?>
