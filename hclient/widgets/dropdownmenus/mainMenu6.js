0dc52bdd530c372f3d8167e147b9fcf89f98852a
Brandon <blmckay13@gmail.com>
2022-05-24 15:43:28 +1000

    Favourite filters - Shorten area height when empty

--- hclient/widgets/dropdownmenus/mainMenu6.js
+++ hclient/widgets/dropdownmenus/mainMenu6.js
@@ -1051,7 +1051,7 @@ $.widget( "heurist.mainMenu6", {
 
                 this.menues.explore.find('.favour-help').hide();
             }else{
-                var cont_height = this.menues.explore.height() - $favourite_container.position().top - 80;
+                var cont_height = this.menues.explore.height() - $favourite_container.position().top - 100;
                 $favourite_container.css('height', cont_height + 'px');
             }
         }
