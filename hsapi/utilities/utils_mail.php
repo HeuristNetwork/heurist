3fb443fe9a060a7644aa14030228334ae3db6eeb
Artem Osmakov <osmakov@gmail.com>
2022-07-08 11:58:37 +0400

    Show server name in default "email from"

--- hsapi/utilities/utils_mail.php
+++ hsapi/utilities/utils_mail.php
@@ -38,7 +38,7 @@ use PHPMailer\PHPMailer\Exception;
         global $system;
 
         if(!$email_from) $email_from = 'no-reply@'.HEURIST_DOMAIN;
-        if(!$email_from_name) $email_from_name = 'Heurist system';
+        if(!$email_from_name) $email_from_name = 'Heurist system. ('.HEURIST_SERVER_NAME.')';
         
         if(is_array($email_text)){
             $email_text =  json_encode($email_text);    
