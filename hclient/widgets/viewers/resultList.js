3e6a5538068a4170fdf1f7b78c376be3ad3d34d0
Brandon <blmckay13@gmail.com>
2022-09-08 15:04:34 +1000

    Result list - Hide visibility eye if not logged in

--- hclient/widgets/viewers/resultList.js
+++ hclient/widgets/viewers/resultList.js
@@ -1650,28 +1650,31 @@ $.widget( "heurist.resultList", {
             html_owner =  '<span class="rec_owner logged-in-only" style="width:20px;padding-top:2px;display:inline-block;color:'
                      + clr + '" title="' + hint + '"><b>' + owner_id + '</b></span>';
 
-            // Display eye if record is publicly viewable, otherwise use a crossed out eye - Both icons come from: iconoir.com
-            var vis_icon = '<svg width="20px" height="20px" stroke-width="1.23" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">'
-                            + '<path d="M12 14a2 2 0 100-4 2 2 0 000 4z" stroke="#000000" stroke-width="1.23" stroke-linecap="round" stroke-linejoin="round"></path>'
-                            + '<path d="M21 12c-1.889 2.991-5.282 6-9 6s-7.111-3.009-9-6c2.299-2.842 4.992-6 9-6s6.701 3.158 9 6z"'
-                                + 'stroke="#000000" stroke-width="1.23" stroke-linecap="round" stroke-linejoin="round"></path>'
-                         + '</svg>';
-            var vis_title = '';
+            if(is_logged){ // hide eye if user not logged in
 
-            if(clr != 'blue'){
+                // Display eye if record is publicly viewable, otherwise use a crossed out eye - Both icons come from: iconoir.com
+                var vis_icon = '<svg width="20px" height="20px" stroke-width="1.23" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">'
+                                + '<path d="M12 14a2 2 0 100-4 2 2 0 000 4z" stroke="#000000" stroke-width="1.23" stroke-linecap="round" stroke-linejoin="round"></path>'
+                                + '<path d="M21 12c-1.889 2.991-5.282 6-9 6s-7.111-3.009-9-6c2.299-2.842 4.992-6 9-6s6.701 3.158 9 6z"'
+                                    + 'stroke="#000000" stroke-width="1.23" stroke-linecap="round" stroke-linejoin="round"></path>'
+                             + '</svg>';
+                var vis_title = window.hWin.HR('Publicly visible');
 
-                vis_icon = '<svg width="20px" height="20px" stroke-width="1.23" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">'
-                            + '<path d="M3 3l18 18M10.5 10.677a2 2 0 002.823 2.823" stroke="#000000" stroke-width="1.23" stroke-linecap="round" stroke-linejoin="round"></path>'
-                            + '<path d="M7.362 7.561C5.68 8.74 4.279 10.42 3 12c1.889 2.991 5.282 6 9 6 1.55 0 3.043-.523 4.395-1.35M12 6c4.008 0 6.701 3.158 9 6a15.66 15.66 0 01-1.078 1.5"'
-                                + 'stroke="#000000" stroke-width="1.23" stroke-linecap="round" stroke-linejoin="round">'
-                         + '</svg>';
+                if(clr != 'blue'){
 
-                vis_title = window.hWin.HR('resultList_private_record');
+                    vis_icon = '<svg width="20px" height="20px" stroke-width="1.23" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#FFA500">'
+                                + '<path d="M3 3l18 18M10.5 10.677a2 2 0 002.823 2.823" stroke="#FFA500" stroke-width="1.23" stroke-linecap="round" stroke-linejoin="round"></path>'
+                                + '<path d="M7.362 7.561C5.68 8.74 4.279 10.42 3 12c1.889 2.991 5.282 6 9 6 1.55 0 3.043-.523 4.395-1.35M12 6c4.008 0 6.701 3.158 9 6a15.66 15.66 0 01-1.078 1.5"'
+                                    + 'stroke="#FFA500" stroke-width="1.23" stroke-linecap="round" stroke-linejoin="round">'
+                             + '</svg>';
+
+                    vis_title = window.hWin.HR('resultList_private_record');
+                }
+                html_owner = html_owner 
+                + '<span title="'+vis_title+'" style="position:relative;top:3px;left:-8px;vertical-align:-1px">'
+                    + vis_icon
+                + '</span>';
             }
-            html_owner = html_owner 
-            + '<span title="'+vis_title+'" style="position:relative;top:3px;left:-8px;vertical-align:-1px">'
-                + vis_icon
-            + '</span>';
         }
         
         var btn_icon_only = window.hWin.HEURIST4.util.isempty(this.options.recordDivClass)
