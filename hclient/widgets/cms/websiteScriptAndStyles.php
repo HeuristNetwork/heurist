fb12a10ec5a901324aa3cf7ed133a00123408b2a
Artem Osmakov <osmakov@gmail.com>
2022-09-22 16:34:40 +0400

    CMS. Remove redundant error message

--- hclient/widgets/cms/websiteScriptAndStyles.php
+++ hclient/widgets/cms/websiteScriptAndStyles.php
@@ -567,7 +567,7 @@ function afterPageLoad(document, pageid, eventdata){
             setTimeout(function(){ afterPageLoad(document, pageid, eventdata) },500);
             return;
         }else{
-            window.hWin.HEURIST4.msg.showMsgErr('Some widgets on this page may not inited properly');
+            //window.hWin.HEURIST4.msg.showMsgErr('Some widgets on this page may not inited properly');
         }
     }
     
