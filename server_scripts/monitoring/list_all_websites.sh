db3d30b55dd29a97d22174cff0fa0ad6bd7ebfd4
ianjohnson <ian.johnson@sydney.edu.au>
2023-05-21 15:25:21 +0200

    Clean up and improve list of websites to generate web page
    The list filters out sites with unconfigured titles ("Enter a website name here" and "UNCONFIGURED SITE TITLE") and creates an html page with links to the websites

--- server_scripts/monitoring/list_all_websites.sh
+++ server_scripts/monitoring/list_all_websites.sh
@@ -2,8 +2,8 @@
 #
 # List CMS Homepage (concept code 99-51) records as link for each Heurist database on this server
 # Input: none
-# Output: a CSV file
-#
+# Output: Simple html list of database website links
+# Updated: Ian Johnson 21 May 2023
 
 
 if ! [ $(id -u) = 0 ]; then
@@ -17,8 +17,10 @@ cd /srv/scripts/results
 
 # Headers insert
 
-echo "Database,Record ID,Website Title" >  list_of_websites_on_all_dbs_temp.txt
+now=$(date)
 
+echo "<html> <body>"  >  list_of_websites_on_all_dbs_temp.txt
+echo "<h1> Heurist websites on $HOSTNAME at $now </h1>"  >>  list_of_websites_on_all_dbs_temp.txt
 
 echo "Processing ..."
 
@@ -27,20 +29,32 @@ for db in `/usr/bin/mysql --login-path=local -N  -u root -pxxxxxx_replace_with_p
 # On Usyd: for db in `/usr/bin/mysql --login-path=local -N -e "select schema_name from information_schema.schemata where schema_name like 'hdb_%'$
 
 do
-  # For all datbases
+  # For all databases
+  # it would be nice to strip out all the formatting except bold and italics from the rec_Title
   /usr/bin/mysql --login-path=local -N -B  -u root -pxxxxxx_replace_with_pwd_xxxxxx  -e " 
-        SELECT \"$db\",\"++++++\",rec_ID,\"++++++\",rec_Title,\"++++++\",\"<a href=https://heurist.huma-num.fr/heurist/?db=$db&website&id=$rec_ID target=_blank>website</a>\"
+        SELECT '<h3><a href=https://xxxxx_replace with server name_xxxxxx/heurist/?db=$db&website&id=',rec_ID,' target=_blank>$db</a></h3>',
+        '<pre style=\"font-size: 10px;margin-left:30px;\">',rec_Title, '</pre>'
         FROM Records
-        WHERE rec_RecTypeID IN (SELECT rty_ID FROM defRecTypes WHERE rty_OriginatingDBID=99 AND rty_IDInOriginatingDB=51) and Not rec_FlagTemporary" $db >> list_of_websites_on_all_dbs_temp.txt;
+        WHERE rec_RecTypeID IN (SELECT rty_ID FROM defRecTypes WHERE rty_OriginatingDBID=99 AND rty_IDInOriginatingDB=51)
+        and Not rec_FlagTemporary
+        and Not rec_Title Like '%Enter a website name here%'
+        and Not rec_Title Like '%UNCONFIGURED SITE TITLE%'" $db >> list_of_websites_on_all_dbs_temp.txt;
+
 
 done
 
 sort list_of_websites_on_all_dbs_temp.txt > list_of_websites_on_all_dbs.tsv
 
+# creates file ready for mailing tgo sysadmin
 rm list_of_websites_on_all_dbs_temp.txt
+
+# Create accessible slightly formatted web page
+cp list_of_websites_on_all_dbs.tsv /var/www/html/HEURIST/list_of_websites_on_all_dbs.html
 
 echo
 echo "Done, use    less /srv/scripts/results/list_of_websites_on_all_dbs.tsv     to view results"
+echo "       or    <server>/HEURIST/list_of_websites_on_all_dbs.html"
+
 echo
 
 
