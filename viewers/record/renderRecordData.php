50067be647321ede0f90856ded8c8e2d5d7a49cc
Artem Osmakov <osmakov@gmail.com>
2023-05-31 15:19:00 +0400

    Fix syntax error in record view

--- viewers/record/renderRecordData.php
+++ viewers/record/renderRecordData.php
@@ -1248,9 +1248,9 @@ function print_public_details($bib) {
                     }
 
                     //find dates
-                    $row = mysql__select_row('SELECT rdi_estMinDate ' 
+                    $row = mysql__select_row($mysqli, 'SELECT rdi_estMinDate ' 
                             .' FROM recDetailsDateIndex'
-                            .' WHERE rdi_RecID=$rec_id AND rdi_DetailTypeID IN ('.DT_DATE.','.$startDT.')'); 
+                            .' WHERE rdi_RecID='.$rec_id .'AND rdi_DetailTypeID IN ('.DT_DATE.','.$startDT.')'); 
 
                     if($row){
                         $bd['order_by_date'] = $row[0];
