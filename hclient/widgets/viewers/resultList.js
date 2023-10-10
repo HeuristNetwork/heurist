8f85d86540d8a39a3fab035d95854961875edb3f
Brandon <blmckay13@gmail.com>
2023-10-10 11:49:12 +1100

    Replace inline SVG with CSS styled icon
    For result lists, replaces the eye opened and crossed out eye inline SVG with the styling defined within h4styles.css (.ui-eye-open and .ui-icon-eye-crossed)

--- hclient/widgets/viewers/resultList.js
+++ hclient/widgets/viewers/resultList.js
@@ -1630,25 +1630,17 @@ $.widget( "heurist.resultList", {
             if(is_logged){ // hide eye if user not logged in
 
                 // Display eye if record is publicly viewable, otherwise use a crossed out eye - Both icons come from: iconoir.com
-                var vis_icon = '<svg width="20px" height="20px" stroke-width="1.23" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">'
-                                + '<path d="M12 14a2 2 0 100-4 2 2 0 000 4z" stroke="#000000" stroke-width="1.23" stroke-linecap="round" stroke-linejoin="round"></path>'
-                                + '<path d="M21 12c-1.889 2.991-5.282 6-9 6s-7.111-3.009-9-6c2.299-2.842 4.992-6 9-6s6.701 3.158 9 6z"'
-                                    + 'stroke="#000000" stroke-width="1.23" stroke-linecap="round" stroke-linejoin="round"></path>'
-                             + '</svg>';
-                var vis_title = window.hWin.HR('Publicly visible');
+                let vis_icon = '<span class="ui-icon ui-icon-eye-open"></span>';
+                let vis_title = window.hWin.HR('Publicly visible');
 
                 if(clr != 'blue'){
 
-                    vis_icon = '<svg width="20px" height="20px" stroke-width="1.23" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#FFA500">'
-                                + '<path d="M3 3l18 18M10.5 10.677a2 2 0 002.823 2.823" stroke="#FFA500" stroke-width="1.23" stroke-linecap="round" stroke-linejoin="round"></path>'
-                                + '<path d="M7.362 7.561C5.68 8.74 4.279 10.42 3 12c1.889 2.991 5.282 6 9 6 1.55 0 3.043-.523 4.395-1.35M12 6c4.008 0 6.701 3.158 9 6a15.66 15.66 0 01-1.078 1.5"'
-                                    + 'stroke="#FFA500" stroke-width="1.23" stroke-linecap="round" stroke-linejoin="round"></path>'
-                             + '</svg>';
+                    vis_icon = '<span class="ui-icon ui-icon-eye-crossed"></span>';
 
                     vis_title = window.hWin.HR('resultList_private_record');
                 }
                 html_owner = html_owner 
-                + '<span title="'+vis_title+'" style="position:relative;top:3px;left:-8px;vertical-align:-1px">'
+                + '<span title="'+vis_title+'" style="position:relative;left:-8px;">'
                     + vis_icon
                 + '</span>';
             }
