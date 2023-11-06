3c442ecd0a60b45c4e469661d6f6c6d8d9beb38a
Artem Osmakov <osmakov@gmail.com>
2023-11-06 13:18:22 +1100

    snyk security updates 2

--- admin/verification/listUploadedFilesMissed.php
+++ admin/verification/listUploadedFilesMissed.php
@@ -99,8 +99,8 @@ $missed_folders = array();
 foreach ($databases as $idx=>$db_name){
 
     //mysql__usedatabase($mysqli, $db_name);
-    $db_name = htmlspecialchars($db_name);
-    $query2 = 'SELECT ulf_FilePath, ulf_FileName FROM `hdb_'.$mysqli->real_escape_string($db_name).'`.recUploadedFiles '
+    $db_name2 = $mysqli->real_escape_string('hdb_'.$db_name);
+    $query2 = 'SELECT ulf_FilePath, ulf_FileName FROM `'.$db_name2.'`.recUploadedFiles '
                     .'WHERE ulf_FileName is not null ORDER BY ulf_FilePath';
                     
     $res2 = $mysqli->query($query2);
