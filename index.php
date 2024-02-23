d6071a32c404a1dfe5b117acf89bbcb757da6dcc
Artem Osmakov <osmakov@gmail.com>
2024-02-23 20:15:42 +0400

    snyk report security fixes #14-18

--- index.php
+++ index.php
@@ -130,7 +130,7 @@ if( @$_REQUEST['recID'] || @$_REQUEST['recid'] || array_key_exists('website', $_
 
     $locale = filter_var(@$_REQUEST['lang'], FILTER_SANITIZE_STRING); //locale
     if($locale && preg_match('/^[A-Za-z]{3}$/', $locale)){
-        $locale = strtolower($locale);
+        $locale = urlencode(strtolower($locale));
         $locale = ($locale=='eng')?'' :($locale.'/');
     }else{
         $locale = '';    
