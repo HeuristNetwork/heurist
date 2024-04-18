b2d1c768e7aee9f235e1ac5e88af3b1611a772e3
Artem Osmakov <osmakov@gmail.com>
2024-04-18 10:38:32 +0400

    folderFirstFile function

--- hserv/entity/dbRecUploadedFiles.php
+++ hserv/entity/dbRecUploadedFiles.php
@@ -767,7 +767,7 @@ When we open "iiif_image" in mirador viewer we generate manifest dynamically.
                             $size = folderSize2($dest);
                             
                             //get first file from first folder - use it as thumbnail
-                            $filename = folderFirstTileImage($dest);
+                            $filename = folderFirstFile($dest);
                             
                             $thumb_name = HEURIST_THUMB_DIR.'ulf_'.$ulf_ObfuscatedFileID.'.png';
                             
--- hserv/utilities/uFile.php
+++ hserv/utilities/uFile.php
@@ -18,12 +18,12 @@
     * folderSize
     * folderTree
     * folderTreeToFancyTree - NOT USED
-    * folderFirstTileImage - returns first file from first folder - for tiled image stack
+    * folderFirstFile - returns first file from first folder (search first file given ext or just first file in subfolders 
+    *                   used for tiled image stack
     *     
     * fileCopy
     * fileSave
     * fileOpen - check existance, readability, opens and returns file handle, or -1 not exist, -2 not readable -3 can't open
-    * fileWithGivenExt - returns basename by filename (extension is known)
     *     
     * getRelativePath
     * folderRecurseCopy
@@ -421,7 +421,7 @@
     //
     //
     //
-    function folderFirstTileImage($dir){
+    function folderFirstFile($dir, $ext=null, $recursion=true){
     
         $dir = realpath($dir);
         
@@ -434,9 +434,23 @@
                 }
                 $file = $dir.'/'.$node;
                 if(is_dir($file)){
-                    return folderFirstTileImage($file);    
+                    if($recursion){
+                        return folderFirstFile($file, $ext, $recursion);    
+                    }
                 }else{
-                    return $file;    
+                    if($ext!=null)
+                    {
+                        $path_parts = pathinfo($file);
+                        if(array_key_exists('extension', $path_parts))
+                        {
+                            $fext = strtolower($path_parts['extension']);
+                            if($ext==$fext){
+                                return $file;    
+                            }
+                        }
+                    }else{
+                        return $file;
+                    }
                 }
             }
         }
