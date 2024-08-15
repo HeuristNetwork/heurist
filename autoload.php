29d25b2388c4b0cd97c7ca09924f0ef6070487b1
Artem Osmakov <osmakov@gmail.com>
2024-08-15 18:00:25 +0400

    Fix autoload function

--- autoload.php
+++ autoload.php
@@ -1,20 +1,22 @@
 <?php
 spl_autoload_register(function ($class) {
+    
     $prefix = 'hserv\\';
     if (strpos($class, $prefix) !== 0) {
         //$prefix = __NAMESPACE__ . $class;
         return;
     }
-    //$filename = str_replace('\\', DIRECTORY_SEPARATOR, substr($class, strlen($prefix))) . '.php';
-    $filename = $class . '.php';
+
+    $filename = str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
     $filepath = __DIR__ . DIRECTORY_SEPARATOR . $filename;
+    
     if (!is_readable($filepath)) {
         return;
     }
     require_once $filepath;
 });
 
-require_once dirname(__FILE__).'../configIni.php';// read in the configuration file
+require_once dirname(__FILE__).'/configIni.php';// read in the configuration file
 require_once dirname(__FILE__).'/hserv/consts.php';
 
 require_once dirname(__FILE__).'/hserv/dbaccess/utils_db.php';
