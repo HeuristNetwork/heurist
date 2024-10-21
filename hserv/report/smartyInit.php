ea5a00b4ab1ecced0f75f376ddb87259b46b5d61
Artem Osmakov <osmakov@gmail.com>
2024-10-21 16:28:02 +0400

    Smarty modifiiers: arraysortby, sort, array_mutisort

--- hserv/report/smartyInit.php
+++ hserv/report/smartyInit.php
@@ -44,7 +44,10 @@ class HeuristSecurityPolicy extends Security {
                     'lower','nl2br',
                     'preg_match_all','print_r', 'printf','replace',
                     'range', 'regex_replace', 'round',
-                    'setlocale','sort', 'spacify', 'strip', 'strstr', 'substr', 'strpos', 'string_format', 'strlen', 'strip_tags',
+                    'sort', 
+                    'setlocale','spacify', 'strcmp',
+                    'strip', 'strstr', 'substr', 'strpos', 'string_format', 'strlen', 'strip_tags',
+                    'arraysortby',
                     'time','translate','truncate',
                     'out','wrap',
                     'upper','utf8_encode','wordwrap');
@@ -53,6 +56,8 @@ class HeuristSecurityPolicy extends Security {
 
   public $allowed_tags = false;
 
+  public $allow_constants = true;
+  
 }
 
 function smartyInit($smarty_templates_dir=null){
@@ -88,19 +93,21 @@ function smartyInit($smarty_templates_dir=null){
         $smarty->setConfigDir($smarty_templates_dir.'configs');
 
         // enable security
+        
         $smarty->enableSecurity('HeuristSecurityPolicy');
-
+        
         //allowed php functions
         $php_functions = array( 'count', //'constant', 
                     'sizeof', 'in_array', 'is_array', 'intval', 'implode', 'explode',
                     //'array_key_exists', 'array_column',
-                    'array_count_values', 'array_keys', 'array_multisort',
+                    'array_count_values', 'array_keys', //'array_multisort',
                     'array_diff', 'array_merge', 'array_slice', 'array_unique',
-                    'array_values', 'asort',
+                    'asort', 'ksort', //'sort', 
+                    'array_values', 
                     'floatval', 'is_numeric', 'json_encode',
-                    'ksort', 'nl2br',
+                    'nl2br',
                     'preg_match_all','print_r','printf', 'range', 'round',
-                    'setlocale', 'sort', 'strpos', 'strstr', 'substr', 'strlen',
+                    'setlocale', 'strcmp', 'strpos', 'strstr', 'substr', 'strlen',
                     'time',
                     'utf8_encode');
 
@@ -112,8 +119,24 @@ function smartyInit($smarty_templates_dir=null){
         $smarty->registerPlugin(Smarty::PLUGIN_MODIFIER, 'array_key_exists', 'heuristModifierArrayKeyExists');
         $smarty->registerPlugin(Smarty::PLUGIN_MODIFIER, 'array_column', 'heuristModifierArrayColumn');
         $smarty->registerPlugin(Smarty::PLUGIN_MODIFIER, 'translate', 'heuristModifierTranslate');
+        $smarty->registerPlugin(Smarty::PLUGIN_MODIFIER, 'arraysortby', 'heuristModifierArraySortByFields');
 
+        $smarty->registerPlugin(Smarty::PLUGIN_MODIFIER, 'sort', 'heuristModifierArraySort');
+        $smarty->registerPlugin(Smarty::PLUGIN_MODIFIER, 'array_multisort', 'heuristModifierArrayMultiSort');
+        
+        
         return $smarty;
+}
+
+
+function heuristModifierArraySort($arr){
+    sort($arr);
+    return $arr;
+}
+
+function heuristModifierArrayMultiSort(...$params){
+    array_multisort(...$params);
+    return end($params);
 }
 
 function heuristModifierArrayKeyExists($key, $arr){
@@ -131,7 +154,77 @@ function heuristModifierTranslate($input, $lang, $field=null)
 {
     return getTranslation($input, $lang, $field);//see ULocale
 }
+/** 
+* Smarty Plugin - Array Sort
+* Sorts an array by supplied fields
+* 
+* $sortby - comma separated string or array of columns/keys
+*           prefix '-' sort desc, prefix '#' - sort numeric
+* 
+* for example 
+*       foreach $arr|arraysortby:"-auteur,#auteurid" 
+* or 
+*       $arr=arraysortby($arr,"auteur")
+* 
+* if $sortby is not defined it sorts
+*   a) by first column for multidimensional array
+*   b) use "sort" function for flat array
+* 
+*/
+function heuristModifierArraySortByFields($arr, $sortby=null) 
+{
+    
+    if(!isset($sortby)){
+        if(is_array($arr[0])){
+            //sort by first column    
+            $sortby = array_keys($arr[0])[0];
+        }else{
+            //flat array
+            sort($arr);    
+            return $arr;    
+        }
+    }
+    if(!is_array($sortby)){
+        $sortby = explode(',',$sortby);
+    }
 
+    uasort($arr, function ($a, $b) use (&$arr, $sortby) {
+         for ($i = 0; $i < count($sortby); $i++) {
+            
+            $key = $sortby[$i];
+
+            $d = 1;
+            if (substr($key,0,1) == '-') {
+                $d = -1;
+                $key = substr($key,1);
+            }
+            $is_numeric = false;
+            if (substr($key,0,1) == '#') {
+                //numeric
+                $is_numeric = true;
+                $key = substr($key,1);
+            }
+            
+            if ($a[$key] == $b[$key]) {
+                if (isset($sortby[$i + 1])) { //next sort
+                    $arr = heuristModifierArraySortByFields($arr, array_slice($sortby, 1));
+                } else {
+                    return 0;
+                }
+            } else {
+                if($is_numeric){
+                    $res = $d * ($a[$key]-$b[$key]);
+                }else{
+                    $res = $d * strcasecmp($a[$key], $b[$key]);
+                }
+                return $res;
+            }
+        }
+    });
+
+    return $arr;
+}
+    
 //do not remove - for backward capability
 function smarty_function_progress(){
     return false;
