bc29df6f146a2b6c26fb671f480b3066ae028500
Brandon <blmckay13@gmail.com>
2025-03-13 21:09:07 +1100

    Add figcaption tags to whitelist [JT #2157]

--- hserv/utilities/USanitize.php
+++ hserv/utilities/USanitize.php
@@ -161,7 +161,7 @@ class USanitize {
             $message = '';
         }else{
             if($allowed_tags==null) {
-                $allowed_tags = '<a><u><i><div><em><b><strong><sup><sub><small><br><h1><h2><h3><h4><h5><h6><p><ul><li><img><blockquote><pre><span><bibl><persName>';
+                $allowed_tags = '<a><u><i><div><em><b><strong><sup><sub><small><br><h1><h2><h3><h4><h5><h6><p><ul><li><img><blockquote><pre><span><bibl><persName><figcaption>';
             }elseif($allowed_tags===false){
                 $allowed_tags = null;
             }
