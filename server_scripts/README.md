3fcb32391d5dde633916f9c9e30acc219f2cd80b
Ian Johnson <ian.johnson.france@gmail.com>
2025-07-13 14:50:32 +0200

    Renaming scripts in server_scripts directory

--- server_scripts/README.md
+++ server_scripts/README.md
@@ -1,26 +1,10 @@
-# Directory: /server_scripts
 
-## Overview
+Directory:	/server_scripts
 
-This directory contains scripts for installation, update, and verification of Heurist instances, as well as management of databases on a server.
+Overview:	Scripts for installation, update and verification of instances
+            and management of databases on a server
 
-## Subfolders
+Created: 	24th Feb 2016, Mostly work in progress @ oct 2022
 
--   `monitoring/`: scripts for monitoring various aspects of the Heurist installation and server, including listing infromation.
--   `safeguard/`:  scripts for backing up, archiving backups, and managing archive space. 
-                   These are typically placed in `/srv/scripts` and called from cron jobs.
-     
--   `utility/`: various utility scripts for managing and maintaining Heurist, mostly old requirements.
--   `experiments/`: experimental scripts and test files.
--   `prerequisites/`: scripts related to installing prerequisites for Heurist. These are older scripts, retained mainly for reference.
+---------------------------------------------------------------------
 
-## Key files
-
--   `apache_configurations.txt`: Example Apache configurations.
--   `crontab file examples.txt`: Example crontab entries for scheduling tasks.
--   `databases_not_to_purge.txt`: List of databases that should not be purged.
--   `js_in_database_authorised.txt`: List of authorized JavaScript in the database.
--   `model_crontab_to_run_scripts.txt`: Model crontab file for running scripts.
--   `model_robots.txt`: Model `robots.txt` file.
--   `virtual_host_configurations.txt`: Example virtual host configurations.
--   'run_checks_and_email_lists_to_support.sh' : generates various useful summaries of databases
---
+++ server_scripts/model_for_webroot_index.php
@@ -0,0 +1,44 @@
+<?php
+
+    /**
+    * move_to_webroot_as_index.php (optional)
+    * use this to redirect the root of the website to the Heurist swichboard, if desired
+\    * You can also do this with html redirect (see below) but it is potentially less reliable
+    *
+    * @package     Heurist academic knowledge management system
+    * @link        https://HeuristNetwork.org
+    * @copyright   (C) 2005-2023 University of Sydney, (C) 2024 onwards Heurist Network
+    * @author      Ian Johnson   <ian.johnson.heurist@gmail.com>
+    * @license     https://www.gnu.org/licenses/gpl-3.0.txt GNU License 3.0
+    * @version     4.0
+    */
+
+    /*
+    * Licensed under the GNU License, Version 3.0 (the "License"); you may not use this file except in compliance
+    * with the License. You may obtain a copy of the License at https://www.gnu.org/licenses/gpl-3.0.txt
+    * Unless required by applicable law or agreed to in writing, software distributed under the License is
+    * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied
+    * See the License for the specific language governing permissions and limitations under the License.
+    */
+
+    header( 'Location: /HEURIST/startup');
+
+
+/*   This is the html alternative - not tested
+
+<!DOCTYPE HTML>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="refresh" content="1; url=/HEURIST/index.html">
+        <script type="text/javascript">
+            window.location.href = "/HEURIST/index.html"
+        </script>
+        <title>Redirect to Heurist switchboard</title>
+    </head>
+    <body>
+        If you are not redirected automatically, follow the <a href='/HEURIST/index.html'>link to example</a>
+    </body>
+</html>
+*/
+?>
