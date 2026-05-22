ff15e37767138c6ad06fc54f7e70ac48813d58cd
Artem Osmakov <osmakov@gmail.com>
2026-05-22 14:25:58 +0400

    Restore custom report link on Record View

--- viewers/record/renderRecordData.php
+++ viewers/record/renderRecordData.php
@@ -1201,6 +1201,11 @@ if(!$system->hasAccess()){
                     }
 
                 }
+                
+                if(isAdminInterface){
+                    $('#custom-report-link').show();
+                    $('#custom-report-link').on('click', () => openReportSelector());    
+                }                
 
             });
 
